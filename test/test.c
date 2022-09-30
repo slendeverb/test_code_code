44001216dbd1ae30288e3e1939bb69bf289d3b4c
stwings <571641990@qq.com>
2022-09-30 16:27:52 +0800

    更新

--- test/test.c
+++ test/test.c
@@ -2,40 +2,26 @@
 
 int main()
 {
-	unsigned int a = 0;
-	unsigned int b = 0;
-	int num = 0;
-	int i = 0;
-	int j = 0;
-	scanf("%d", &num);
-	unsigned int arr[20] = { 0 };
-	char ch[10] = { 0 };
-	for (i = 0; i < num; i++)
+	unsigned int a,b;
+	char ch;
+	while (scanf("%d%c%d", &a, &ch, &b) != EOF)
 	{
-		scanf("%u%c%u", &arr[j], &ch[i], &arr[j + 1]);
-		j += 2;
-	}
-	j = 0;
-	for (i = 0; i < num; i++)
-	{
-		a = arr[j];
-		b = arr[j + 1];
-		switch (ch[i])
+		if (ch == '+')
 		{
-		case '+':
 			printf("%d\n", a + b);
-			break;
-		case '-':
-			printf("%d\n", a - b);
-			break;
-		case '*':
-			printf("%d\n", a * b);
-			break;
-		case '/':
-			printf("%d\n", a / b);
-			break;
 		}
-		j += 2;
+		if (ch == '-')
+		{
+			printf("%d\n", a - b);
+		}
+		if (ch == '*')
+		{
+			printf("%d\n", a * b);
+		}
+		if (ch == '/')
+		{
+			printf("%d\n", a / b);
+		}
 	}
 	return 0;
 }
