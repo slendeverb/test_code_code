38c44ec8a85732ea9bde238a5c46c87bcc790676
stwings <571641990@qq.com>
2022-11-04 21:37:08 +0800

    update

---
+++ C++/Atcoder/problems/AtCoder Beginner Contest/AtCoder Beginner Contest 139/B - Power Socket/B - Power Socket(2).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0, b = 0;
+    cin >> a >> b;
+    if (b == 1)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        if (b % a == 0)
+        {
+            cout << b / a << endl;
+        }
+        else
+        {
+            cout << b / a + 1 << endl;
+        }
+    }
+    return 0;
+}
