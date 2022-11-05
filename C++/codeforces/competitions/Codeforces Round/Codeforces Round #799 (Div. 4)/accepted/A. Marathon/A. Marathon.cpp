bf6ed4a27ed6c8bcf1a8f7425fd9e4f29fc0088e
stwings <571641990@qq.com>
2022-11-05 13:22:34 +0800

    update

--- C++/codeforces/competitions/Codeforces Round/Codeforces Round #799 (Div. 4)/accepted/A. Marathon/A. Marathon.cpp
+++ C++/codeforces/competitions/Codeforces Round/Codeforces Round #799 (Div. 4)/accepted/A. Marathon/A. Marathon.cpp
@@ -7,7 +7,7 @@ int main()
     cin >> t;
     while (t--)
     {
-        int a = 0, b = 0, c = 0, d = 0;
+        /*int a = 0, b = 0, c = 0, d = 0;
         cin >> a >> b >> c >> d;
         int count = 0;
         if (b > a)
@@ -22,7 +22,17 @@ int main()
         {
             count++;
         }
-        cout << count << endl;
+        cout << count << endl;*/
+        int n, m;
+        scanf("%d", &n);
+
+        int res = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            scanf("%d", &m);
+            if (m > n)
+                res++;
+        }
     }
     return 0;
 }
