f7669c0e5bb48ec1545fce506e0651e1d28f08e4
stwings <571641990@qq.com>
2022-11-06 22:30:44 +0800

    update

---
+++ C++/codeforces/competitions/Codeforces Round/Codeforces Round #805 (Div. 3)/accepted/D. Not a Cheap String/D. Not a Cheap String(4)(copy).cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    map<int, pair<int, int>> m;
+    forn(i, n)
+    {
+        int u;
+        cin >> u;
+        if (!m.count(u))
+        {
+            m[u].first = i;
+            m[u].second = i;
+        }
+        else
+            m[u].second = i;
+    }
+    forn(i, k)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (!m.count(a) || !m.count(b) || m[a].first > m[b].second)
+        {
+            cout << "NO\n"; // equals = 0 = wrong
+        }
+        else
+            cout << "YES\n";
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
