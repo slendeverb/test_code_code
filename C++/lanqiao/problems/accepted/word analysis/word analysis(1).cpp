36b462c5ce1b60b7be560b75ed78011d130092bb
stwings <571641990@qq.com>
2022-11-10 23:10:43 +0800

    update

---
+++ C++/lanqiao/problems/accepted/word analysis/word analysis(1).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+int main()
+{
+    // 请在此输入您的代码
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    for (int i = 0; i < s.size(); i++)
+    {
+        mp[s[i]]++;
+    }
+    int ans = 0;
+    char ret = '0';
+    for (auto it : mp)
+    {
+        if (ans < it.second)
+        {
+            ans = it.second;
+            ret = it.first;
+        }
+    }
+    cout << ret << endl;
+    cout << ans << endl;
+    return 0;
+}
