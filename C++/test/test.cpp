cec9394928d9b4eb471323c81b7a59543c897961
stwings <571641990@qq.com>
2022-11-17 10:12:07 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,19 +1,8 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    char a[] = "morning";
-
-    int i, j = 0;
-
-    for (i = 1; i < 7; i++)
-
-        if (a[j] < a[i])
-            j = i;
-
-    a[j] = a[7];
-
-    puts(a);
+    
     return 0;
 }
