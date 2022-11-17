ea80233b198d4a27871129a0a5739d5400d31a01
stwings <571641990@qq.com>
2022-11-17 10:11:52 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,8 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    
+    char a[] = "morning";
+
+    int i, j = 0;
+
+    for (i = 1; i < 7; i++)
+
+        if (a[j] < a[i])
+            j = i;
+
+    a[j] = a[7];
+
+    puts(a);
     return 0;
 }
