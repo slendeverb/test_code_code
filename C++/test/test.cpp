c8117b71104f4801d8bbd002e6c5d62741086286
stwings <571641990@qq.com>
2022-11-17 10:45:01 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std;
-#include<cstring>
+
 int main()
 {
-    char a[20] = "ABCD\0EFG\0", b[] = "IJK";
-
-    strcat(a, b);
-    printf("%s\n", a);
+    
     return 0;
 }
