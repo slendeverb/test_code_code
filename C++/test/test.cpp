8016361de6dafee0b01f09d7e555d2933874c8ba
stwings <571641990@qq.com>
2022-11-17 10:14:07 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
+#include<cstring>
 int main()
 {
-    
+    char a[20] = "ABCD\0EFG\0", b[] = "IJK";
+
+    strcat(a, b);
+    printf("%s\n", a);
     return 0;
 }
