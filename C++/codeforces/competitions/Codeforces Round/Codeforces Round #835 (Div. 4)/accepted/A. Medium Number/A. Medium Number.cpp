31aab312ebdb480fcbe0ea81eaeb09e0135bc716
stwings <571641990@qq.com>
2022-11-22 00:43:15 +0800

    update

--- C++/codeforces/competitions/Codeforces Round/Codeforces Round #835 (Div. 4)/accepted/A. Medium Number/A. Medium Number.cpp
+++ C++/codeforces/competitions/Codeforces Round/Codeforces Round #835 (Div. 4)/accepted/A. Medium Number/A. Medium Number.cpp
@@ -5,12 +5,17 @@ typedef long long ll;
 
 void solve()
 {
-    
+    int a, b, c;
+    cin >> a >> b >> c;
+    int ma = max(a, max(b, c));
+    int mi = min(a, min(b, c));
+    int ans = ma ^ mi ^ a ^ b ^ c;
+    cout<<ans<<"\n";
 }
 
 int main()
 {
-    //ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(0);
 
     int t;
