b19846bc442b0d3fdcd727a4bf04733912654cc2
stwings <571641990@qq.com>
2022-11-24 15:34:52 +0800

    update

---
+++ C++/template/UFSets(Disjoint Sets).cpp
@@ -0,0 +1,41 @@
+void InitUFSets(int S[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        S[i] = -1;
+    }
+}
+
+int Find(int S[], int x)
+{
+    int root = x;
+    while (S[root] >= 0)
+    {
+        root = S[root];
+    }
+    while (x != root)
+    {
+        int t = S[x];
+        S[x] = root;
+        x = t;
+    }
+    return root;
+}
+
+void Union(int S[], int root1, int root2)
+{
+    if (root1 == root2)
+    {
+        return;
+    }
+    if (S[root1] < S[root2])
+    {
+        S[root1] += S[root2];
+        S[root2] = root1;
+    }
+    else
+    {
+        S[root2] += S[root1];
+        S[root1] = root2;
+    }
+}
