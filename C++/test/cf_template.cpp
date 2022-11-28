af3cf4f41c582a42ee79c1f018d12fa6a4407e8b
stwings <571641990@qq.com>
2022-11-28 13:17:28 +0800

    update

--- C++/test/cf_template.cpp
+++ C++/test/cf_template.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define IOS                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
 typedef long long LL;
 #define ll LL
 
@@ -17,8 +22,7 @@ void solve()
 
 int main()
 {
-    //ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-
+    //IOS;
     cin >> t;
     while (t--)
         solve();
