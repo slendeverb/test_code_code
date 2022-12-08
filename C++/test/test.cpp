13ab86da51a0cc1b80152c95c2780e6ef06a6593
stwings <571641990@qq.com>
2022-12-08 13:58:59 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,43 +1,8 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-void fun(char *a)
-{
-    char *p1 = a, *p2 = a + strlen(a) - 1;
-    while (*p1 == '*')
-    {
-        p1++;
-    }
-    p1++;
-    while (*p2 == '*')
-    {
-        p2--;
-    }
-    p2++;
-    char *p3 = p1;
-    while (p3 < p2)
-    {
-        if (*p3 == '*')
-        {
-            p3++;
-        }
-        else
-        {
-            *p1 = *p3;
-            p1++, p3++;
-        }
-    }
-    *p1 = '\0';
-}
 int main()
 {
-    char s[81];
-    FILE *in, *out;
-    int i;
-    printf("Enter a string :\n");
-    gets(s);
-    fun(s);
-    printf("The string after deleted:\n");
-    puts(s);
+    
     return 0;
 }
