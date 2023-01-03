d81d51bab6edd00a0685f510ec2accb01fe7b8b8
stwings <571641990@qq.com>
2023-01-03 21:54:49 +0800

    update

--- C++/template/quickpow/quickpow.cpp
+++ C++/template/quickpow/quickpow.cpp
@@ -13,3 +13,18 @@ int qpow(int x, int b, int mod)
     else
         return tmp * x % mod;
 }
+
+const int p = 1e9 + 7;
+
+inline int qpow(long long a, int b)
+{
+    int ans = 1;
+    a = (a % p + p) % p;
+    for (; b; b >>= 1)
+    {
+        if (b & 1)
+            ans = (a * ans) % p;
+        a = (a * a) % p;
+    }
+    return ans;
+}
