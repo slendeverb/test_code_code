35b75d50f652ac36eba74b45280154befea566fc
stwings <571641990@qq.com>
2023-01-04 14:56:59 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,19 +1,8 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    while (n--)
-    {
-        int L;
-        cin >> L;
-        int x1, y1;
-        cin >> x1 >> y1;
-        int x2, y2;
-        cin >> x2, y2;
-        
-    }
+    
     return 0;
 }
