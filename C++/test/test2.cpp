0607b0b8a1977b5101015136fc450de0bcee85f4
stwings <571641990@qq.com>
2023-01-07 18:39:12 +0800

    update

---
+++ C++/test/test2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string pos, password;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        cin >> pos >> password;
+        
+    }
+    return 0;
+}
