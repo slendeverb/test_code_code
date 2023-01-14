a56ccbccf3f06d0f396531ea8d8bc1d108ab7f1c
stwings <571641990@qq.com>
2023-01-14 18:09:56 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,22 +1,8 @@
-#include <iostream>
-#include <stack>
-#include <utility>
+#include<iostream>
 using namespace std;
-
-const int N = 3e6 + 10;
-int n, a[N], ans[N];
-stack<pair<int, int>> st;
 
 int main()
 {
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        cin>>a[i];
-    }
-    for(int i=1;i<=n;i++)
-    {
-        
-    }
+    
     return 0;
 }
