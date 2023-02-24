a81208524babbc5238bf9baf451068b5e14d8c23
stwings <571641990@qq.com>
2023-02-24 10:47:20 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
 int main()
 {
-    
+    int secretNumber, guess;
+    srand(time(0));
+    secretNumber = rand() % 100 + 1;
+    cout << "Guess the number (1 to 100): ";
+    cin >> guess;
+    while (guess != secretNumber)
+    {
+        if (guess > secretNumber)
+            cout << "Too high! Try again: ";
+        else
+            cout << "Too low! Try again: ";
+        cin >> guess;
+    }
+    cout << "You guessed it! The number was " << secretNumber << endl;
     return 0;
 }
