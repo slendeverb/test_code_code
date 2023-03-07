7f972f70093a02b04aa60f471e3c3bbc134740d0
stwings <571641990@qq.com>
2023-03-07 21:45:05 +0800

    update

---
+++ C++/lanqiao/problems/accepted/1463 cargo placement/cargo placement.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+typedef long long LL;
+
+int main()
+{
+    long long n = 2021041820210418l;
+    vector<LL> v;
+
+    for (LL i = 1; i * i <= n; i++)
+        if (n % i == 0)
+        {
+            v.push_back(i);
+            if (n / i != i)
+            {
+                v.push_back(n / i);
+            }
+        }
+
+    LL ans = 0;
+    for (auto i : v)
+        for (auto j : v)
+            for (auto k : v)
+                if (i * j * k == n)
+                    ans++;
+
+    cout << ans << '\n';
+    return 0;
+}
--- C++/lanqiao/problems/accepted/819 incremental sequence/incremental sequence.cpp
+++ C++/lanqiao/problems/accepted/819 incremental sequence/incremental sequence.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 vector<string> v;
