5ff784c76007c52b4cc1658ceeaf568d3c1d6730
stwings <571641990@qq.com>
2023-04-16 22:53:20 +0800

    update

--- C++/test/test2.cpp
+++ C++/test/test2.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include <algorithm>
-#include <map>
-#include <iterator>
 using namespace std;
 
 const int maxn = 2e5 + 10;
 int a[maxn];
-map<int, int, less<int>> m;
-bool vis[maxn];
 
 int main()
 {
@@ -17,75 +12,11 @@ int main()
     {
         int n, A, B;
         cin >> n >> A >> B;
-        long long ans = 0;
         for (int i = 1; i <= n; i++)
         {
             cin >> a[i];
         }
-        sort(a + 1, a + 1 + n);
-        for (int i = 1; i <= n; i++)
-        {
-            if (m[a[i]] == 0)
-            {
-                m[a[i]] = upper_bound(a + 1, a + 1 + n, a[i]) - lower_bound(a + 1, a + 1 + n, a[i]);
-            }
-        }
-        auto it = m.begin();
-        if (it->second <= 1)
-        {
-            if (n % 2 == 0)
-            {
-                ans = A * n / 2;
-            }
-            else
-            {
-                ans = -1;
-            }
-        }
-        else if (it->second == n)
-        {
-            ans = -1;
-        }
-        else
-        {
-            auto it = m.begin();
-            if (n % 2 == 1)
-            {
-                ans += B;
-                n--;
-                it->second--;
-                it = m.begin();
-            }
-            while (it->second > n - (it->second))
-            {
-                ans += B;
-                n--;
-                it->second--;
-                it = m.begin();
-            }
-            while (n >= 2 && n % 2 == 0 && it->second >= 2)
-            {
-                long long tmp1 = ans + (A * n / 2);
-                long long tmp2 = ans + (2 * B) + (A * (n - 2) / 2);
-                ans = min(tmp1, tmp2);
-                if (ans == tmp1)
-                {
-                    break;
-                }
-                else if (ans == tmp2)
-                {
-                    n -= 2;
-                    it->second -= 2;
-                    it = m.begin();
-                }
-            }
-            it = m.begin();
-            if (it->second < 2)
-            {
-                ans += (A * n / 2);
-            }
-        }
-        cout << ans << "\n";
+        
     }
     return 0;
 }
