736cafe962f86b7d1fe0343104ac1bd30436eb4b
stwings <571641990@qq.com>
2023-04-19 22:18:24 +0800

    update

--- C++/template/number theory/multiplication inverse/multiplication inverse.cpp
+++ C++/template/number theory/multiplication inverse/multiplication inverse.cpp
@@ -1,37 +1,30 @@
 typedef long long ll;
 const int N = 5e6 + 10;
-const int p = 1e9 + 7;
+const int mod = 1e9 + 7;
 ll inv[N], n, a[N], s[N], sv[N];
 
-void exgcd(int a, int b, int &x, int &y)
+int exgcd(int a, int b, int &x, int &y)
 {
     if (b == 0)
     {
         x = 1, y = 0;
-        return;
-    }
-    exgcd(b, a % b, y, x);
-    y -= a / b * x;
-}
-int ext_gcd(int a, int b, int &x, int &y)
-{
-    if (b == 0)
-    {
-        x = 1;
-        y = 0;
         return a;
     }
-    int d = ext_gcd(b, a % b, x, y);
-    int tmp = y;
-    y = x - a / b * y;
-    x = tmp;
-    return d;
+    int d = exgcd(b, a % b, x, y);
+    int z = x;
+    x = y;
+    y = z - y * (a / b);
+    if (d < 0)
+    {
+        d = -d, x = -x, y = -y;
+    }
+    return d; // d = gcd(a, b), d > 0;
 }
 int inverse1(int a, int b)
 {
     int x, y;
-    int d = ext_gcd(a, b, x, y);
-    x = (x % p + p) % p;
+    int d = exgcd(a, b, x, y);
+    x = (x % mod + mod) % mod;
     return x;
 }
 
@@ -54,7 +47,7 @@ void inverse()
     inv[1] = 1;
     for (int i = 2; i <= n; ++i)
     {
-        inv[i] = (ll)(p - p / i) * inv[p % i] % p;
+        inv[i] = (ll)(mod - mod / i) * inv[mod % i] % mod;
     }
 }
 
@@ -62,11 +55,11 @@ void inverse2()
 {
     s[0] = 1;
     for (int i = 1; i <= n; ++i)
-        s[i] = s[i - 1] * a[i] % p;
-    sv[n] = qpow(s[n], p - 2, p);
+        s[i] = s[i - 1] * a[i] % mod;
+    sv[n] = qpow(s[n], mod - 2, mod);
     // 当然这里也可以用 exgcd 来求逆元,视个人喜好而定.
     for (int i = n; i >= 1; --i)
-        sv[i - 1] = sv[i] * a[i] % p;
+        sv[i - 1] = sv[i] * a[i] % mod;
     for (int i = 1; i <= n; ++i)
-        inv[i] = sv[i] * s[i - 1] % p;
+        inv[i] = sv[i] * s[i - 1] % mod;
 }
