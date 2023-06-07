ca2fa5e6a9c61702dd390a117c124f7cb5279fd6
stwings <571641990@qq.com>
2023-06-07 11:44:25 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    
+    string s = "13811111111";
+    string sub = "6" + s.substr(s.size() - 5, 5);
+    cout << sub << "\n";
     return 0;
 }
