07204fbd93f421c340e90404c30241f37ba92ea5
stwings <571641990@qq.com>
2023-07-17 21:04:11 +0800

    update

---
+++ C++/nowcoder/competitions/2023 summer camp/summer camp 1/accepted/D. Chocolate/D. Chocolate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    if (n == 1 && m == 1)
+    {
+        cout << "Walk Alone\n";
+    }
+    else
+    {
+        cout << "Kelin\n";
+    }
+    return 0;
+}
