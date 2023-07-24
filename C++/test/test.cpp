8a638001692aa39df1cecfeebf939e35ea51737c
stwings <571641990@qq.com>
2023-07-24 18:22:34 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,8 +1,71 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
 using namespace std;
+
+vector<int> tmp = {1, 2, 3, 4, 5, 6, 7, 8};
+map<long long, long long> m;
+
+bool judge(int before, int after)
+{
+    if (before <= tmp.size() / 2)
+    {
+        if (before < after)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        if (before > after)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
 
 int main()
 {
-    
+    long long sum = 0;
+    do
+    {
+        int pos = 0;
+        sum++;
+        long long ans = 0;
+        ans++;
+        int before = 0;
+        int after = 0;
+        do
+        {
+            if (pos == tmp.size() - 1)
+            {
+                break;
+            }
+            before = tmp[pos++];
+            after = tmp[pos];
+            ans++;
+            sum++;
+        } while (judge(before, after));
+        m[ans]++;
+    } while (next_permutation(tmp.begin(), tmp.end()));
+    long long p = 1;
+    for (int i = 1; i <= tmp.size(); i++)
+    {
+        p *= i;
+    }
+    cout << sum << "\n";
+    for (auto e : m)
+    {
+        cout << e.first << ' ' << e.second << "\n";
+    }
     return 0;
 }
--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,80 +1,151 @@
-// Set up the canvas and context
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-// Set up the game variables
-const snakeSize = 10;
-let snake = [{x: 50, y: 50}];
-let food = {x: 0, y: 0};
-let direction = "right";
-let score = 0;
-
-// Set up the game loop
-setInterval(gameLoop, 100);
-
-// Generate a random position for the food
-function generateFood() {
-  food.x = Math.floor(Math.random() * (canvas.width / snakeSize)) * snakeSize;
-  food.y = Math.floor(Math.random() * (canvas.height / snakeSize)) * snakeSize;
+const communication = require("readline/promises").createInterface({
+	input: process.stdin,
+	output: process.stdout,
+})
+const map = Array.from(new Array(15), () => {
+	return new Array(15).fill(-1)
+})
+/**
+ * 向评测机发送落子请求
+ * @arg {number} gamer 已落子的当前玩家
+ * @arg {number} x 落子横坐标
+ * @arg {number} y 落子纵坐标
+ */
+function gamerRequest(gamer, x, y) {
+	const content = {}
+	Reflect.set(content, String(gamer ^ 1), `${x} ${y}`)
+	console.log(JSON.stringify({
+		command: "request",
+		display: JSON.stringify({
+			gamer,
+			x,
+			y,
+		}),
+		content,
+	}))
 }
-
-// Handle keyboard input
-document.addEventListener("keydown", function(event) {
-  if (event.keyCode === 37 && direction !== "right") {
-    direction = "left";
-  }
-  if (event.keyCode === 38 && direction !== "down") {
-    direction = "up";
-  }
-  if (event.keyCode === 39 && direction !== "left") {
-    direction = "right";
-  }
-  if (event.keyCode === 40 && direction !== "up") {
-    direction = "down";
-  }
-});
-
-// Main game loop
-function gameLoop() {
-  // Move the snake
-  let head = {x: snake[0].x, y: snake[0].y};
-  if (direction === "right") {
-    head.x += snakeSize;
-  }
-  if (direction === "left") {
-    head.x -= snakeSize;
-  }
-  if (direction === "up") {
-    head.y -= snakeSize;
-  }
-  if (direction === "down") {
-    head.y += snakeSize;
-  }
-  snake.unshift(head);
-
-  // Check for collision with the walls
-  if (snake[0].x < 0 || snake[0].x >= canvas.width || snake[0].y < 0 || snake[0].y >= canvas.height) {
-    alert("Game over! Your score was " + score);
-    location.reload();
-  }
-
-  // Check for collision with the food
-  if (snake[0].x === food.x && snake[0].y === food.y) {
-    generateFood();
-    score += 10;
-  } else {
-    snake.pop();
-  }
-
-  // Draw the game
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "green";
-  for (let i = 0; i < snake.length; i++) {
-    ctx.fillRect(snake[i].x, snake[i].y, snakeSize, snakeSize);
-  }
-  ctx.fillStyle = "red";
-  ctx.fillRect(food.x, food.y, snakeSize, snakeSize);
-  ctx.fillStyle = "white";
-  ctx.font = "20px Arial";
-  ctx.fillText("Score: " + score, 10, 30);
+/**
+ * 向评测机发送游戏结束信息
+ * @arg {number} gamer 当前玩家
+ * @arg {number} x 落子横坐标
+ * @arg {number} y 落子纵坐标
+ * @arg {string} error 错误信息，若非空则当前玩家判负
+ */
+function gamerWin(gamer, x, y, error = "") {
+	const content = {}
+	const winner = gamer ^ Boolean(error)
+	Reflect.set(content, String(winner & 1), 1)
+	Reflect.set(content, String((winner & 1) ^ 1), 0)
+	console.log(JSON.stringify({
+		command: "finish",
+		display: JSON.stringify({
+			gamer,
+			x,
+			y,
+			winner,
+			error,
+			map,
+		}),
+		content,
+	}))
 }
+/**
+ * 判断几连子，若五连子则结束游戏
+ * @arg {number} count 连子数
+ * @arg {number} gamer 当前玩家
+ * @arg {number} i 判断横坐标
+ * @arg {number} j 判断纵坐标
+ * @arg {number} x 落子横坐标
+ * @arg {number} y 落子纵坐标
+ * @return count 的新值，若游戏结束则返回 undefined
+ */
+function solve(count, gamer, i, j, x, y) {
+	if (gamer != map[i][j]) {
+		return 0
+	}
+	if (count < 4) {
+		return count + 1
+	}
+	gamerWin(gamer, x, y)
+}
+(async () => {
+	gamerRequest(1, -1, -1)
+	for (let gamer = 0; true; gamer ^= 1) {
+		/**
+		 * 当前回合玩家的操作
+		 * @prop raw 操作内容，即 bot 的输出
+		 * @prop verdict 操作结果，若为“OK”则表示操作成功，否则表示操作失败（TLE 之类的）
+		 * @type {{verdict: string, raw: string}}
+		 */
+		const gamerOperation = JSON.parse(await communication.question(""))[String(gamer)]
+		if (!gamerOperation || gamerOperation.verdict != "OK" || typeof gamerOperation.raw != "string") {
+			gamerWin(gamer, -1, -1, "异常退出")
+			break
+		}
+		let [x, y] = gamerOperation.raw.split(" ").map(s => {
+			return parseInt(s)
+		})
+		if (x < 0 || x > 14 || y < 0 || y > 14 || map[x][y] != -1) {
+			gamerWin(gamer, x, y, "落点违规")
+			break
+		}
+		map[x][y] = gamer
+		for (let i = 0; i <= 14; i++) {
+			let count = 0
+			for (let j = 0; j <= 14; j++) {
+				count = solve(count, gamer, i, j, x, y)
+				if (count == undefined) {
+					return
+				}
+			}
+		}
+		for (let j = 0; j <= 14; j++) {
+			let count = 0
+			for (let i = 0; i <= 14; i++) {
+				count = solve(count, gamer, i, j, x, y)
+				if (count == undefined) {
+					return
+				}
+			}
+		}
+		for (let k = 4; k <= 14; k++) {
+			let count = 0
+			for (let j = k; j >= 0; j--) {
+				count = solve(count, gamer, k - j, j, x, y)
+				if (count == undefined) {
+					return
+				}
+			}
+		}
+		for (let k = 1; k <= 10; k++) {
+			let count = 0
+			for (let i = k; i <= 14; i++) {
+				count = solve(count, gamer, i, k - i + 14, x, y)
+				if (count == undefined) {
+					return
+				}
+			}
+		}
+		for (let k = 10; k >= 0; k--) {
+			let count = 0
+			for (let j = k; j <= 14; j++) {
+				count = solve(count, gamer, j - k, j, x, y)
+				if (count == undefined) {
+					return
+				}
+			}
+		}
+		for (let k = 1; k <= 10; k++) {
+			let count = 0
+			for (let i = k; i <= 14; i++) {
+				count = solve(count, gamer, i, i - k, x, y)
+				if (count == undefined) {
+					return
+				}
+			}
+		}
+		gamerRequest(gamer, x, y)
+	}
+})().finally(() => {
+	communication.close()
+})
