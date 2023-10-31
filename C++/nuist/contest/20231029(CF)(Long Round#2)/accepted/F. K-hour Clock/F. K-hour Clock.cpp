7f0e581a48e42574e235563c3de5ca9d93a7114f
stwings <571641990@qq.com>
2023-10-31 23:21:20 +0800

    update

--- C++/nuist/contest/20231029(CF)(Long Round#2)/accepted/F. K-hour Clock/F. K-hour Clock.cpp
+++ C++/nuist/contest/20231029(CF)(Long Round#2)/accepted/F. K-hour Clock/F. K-hour Clock.cpp
@@ -4,9 +4,24 @@ using namespace std;
 #define int long long
 #define endl "\n"
 
+int x, y, z;
+
 void solve()
 {
-    
+    cin >> x >> y >> z;
+    int d = x + y - z;
+    if (d <= max(x, z) && d != 0)
+    {
+        cout << "-1\n";
+    }
+    else if (d == 0)
+    {
+        cout << z + 1 << endl;
+    }
+    else
+    {
+        cout << d << endl;
+    }
 }
 
 signed main()
@@ -14,7 +29,7 @@ signed main()
     // ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
     FILE *in = freopen("./test/in.txt", "r", stdin), *out = freopen("./test/out.txt", "w", stdout);
     int T = 1;
-    // cin >> T;
+    cin >> T;
     // init();
     while (T--)
     {
---
+++ test/test.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class test {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("./test/in.txt"));
+        PrintStream printStream = new PrintStream(new File("./test/out.txt"));
+        PrintStream oldPrintStream = System.out;
+        System.setOut(printStream);
+        int T = in.nextInt();
+        while (T-- != 0) {
+            solve(in);
+        }
+        in.close();
+        printStream.flush();
+        printStream.close();
+        System.setOut(oldPrintStream);
+
+    }
+
+    private static void solve(Scanner in) {
+        int n = in.nextInt();
+        ArrayList<Integer> arrayListOfN = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            arrayListOfN.add(in.nextInt());
+        }
+        for (Integer integer : arrayListOfN) {
+            System.out.print(integer.toString() + " ");
+        }
+        System.out.println();
+
+        int m = in.nextInt();
+        ArrayList<Integer> arrayListOfM = new ArrayList<>();
+        for (int i = 1; i <= m; i++) {
+            arrayListOfM.add(in.nextInt());
+        }
+        for (Integer integer : arrayListOfM) {
+            System.out.print(integer.toString() + " ");
+        }
+        System.out.println();
+
+    }
+
+}
