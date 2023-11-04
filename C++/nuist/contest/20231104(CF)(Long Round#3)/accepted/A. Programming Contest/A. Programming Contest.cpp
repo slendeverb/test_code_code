b762b257411ac7b51772e5ca5aaf70b4fbbc0539
stwings <571641990@qq.com>
2023-11-04 18:22:24 +0800

    update

--- C++/nuist/contest/20231104(CF)(Long Round#3)/accepted/A. Programming Contest/A. Programming Contest.cpp
+++ C++/nuist/contest/20231104(CF)(Long Round#3)/accepted/A. Programming Contest/A. Programming Contest.cpp
@@ -4,9 +4,29 @@ using namespace std;
 #define int long long
 #define endl "\n"
 
+const int N = 105;
+int y1, y2;
+int n;
+int s[N];
+
 void solve()
 {
-    
+    cin >> y1;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> s[i];
+    }
+    cin >> y2;
+    int ans = y2 - y1 + 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (s[i] >= y1 && s[i] <= y2)
+        {
+            ans--;
+        }
+    }
+    cout << ans << endl;
 }
 
 signed main()
