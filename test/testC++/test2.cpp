389fe596b98425532de9b7d81c9bab6f1c444413
stwings <571641990@qq.com>
2024-02-28 17:06:02 +0800

    update

--- test/testC++/test2.cpp
+++ test/testC++/test2.cpp
@@ -34,15 +34,80 @@
 #include <vector>
 //
 using namespace std;
+template <typename T>
+class Grid 
+{
+public:
+    explicit Grid(size_t width = DefaultWidth,size_t height = DefaultHeight);
+    virtual ~Grid() = default;
 
+    Grid(const Grid& src) = default;
+    Grid& operator=(const Grid& rhs) = default;
+
+    Grid(Grid&& src) = default;
+    Grid& operator=(Grid&& rhs) = default;
+
+    std::optional<T>& at(size_t x, size_t y);
+    const std::optional<T>& at(size_t x,size_t y) const;
+    
+    size_t getHeight() const {return m_height;}
+    size_t getWidth() const {return m_width;}
+
+    static const size_t DefaultWidth{10},DefaultHeight{10};
+
+private:
+    void verifyCoornidate(size_t x, size_t y) const;
+
+    std::vector<std::vector<std::optional<T>>> m_cells;
+    size_t m_width{0},m_height{0};
+};
+
+template <typename T>
+Grid<T>::Grid(size_t width,size_t height)
+    :m_width{width},m_height{height}
+{
+    m_cells.resize(m_width);
+    for(auto& column:m_cells)
+    {
+        column.resize(m_height);
+    }
+}
+template <typename T>
+void Grid<T>::verifyCoornidate(size_t x,size_t y) const 
+{
+    if(x>=m_width)
+    {
+        throw std::out_of_range{std::format("{} must be less than {}.",x,m_width)};
+    }
+    if(y>=m_height)
+    {
+        throw std::out_of_range{std::format("{} must be less than {}.",y,m_height)};
+    }
+}
+template <typename T>
+const std::optional<T>& Grid<T>::at(size_t x, size_t y) const
+{
+    this->verifyCoornidate(x,y);
+    return m_cells[x][y];
+}
+template <typename T>
+std::optional<T>& Grid<T>::at(size_t x,size_t y) 
+{
+    return const_cast<std::optional<T>&>(std::as_const(*this).at(x,y));
+}
 void solve()
 {
-    
+    Grid<int> myIntGrid;
+    Grid<double> myDoubleGrid{11,11};
+    myIntGrid.at(0,0) = 10;
+    int x{myIntGrid.at(0,0).value_or(0)};
+    Grid<int> grid2{myIntGrid};
+    Grid<int> anotherIntGrid;
+    anotherIntGrid = grid2;
 }
 
 int main()
 {
-    std::cout << std::endl;
     clock_t startTime{clock()};
     solve();
     clock_t endTime{clock()};
