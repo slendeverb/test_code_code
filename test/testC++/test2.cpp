5194ed522510e03752f7994bfe364c642b09cef4
stwings <571641990@qq.com>
2024-02-29 23:28:11 +0800

    update

--- test/testC++/test2.cpp
+++ test/testC++/test2.cpp
@@ -35,77 +35,112 @@
 //
 using namespace std;
 template <typename T>
-class Grid 
+class Grid
 {
 public:
-    explicit Grid(size_t width = DefaultWidth,size_t height = DefaultHeight);
+    explicit Grid(size_t width = DefaultWidth, size_t height = DefaultHeight);
     virtual ~Grid() = default;
 
-    Grid(const Grid& src) = default;
-    Grid& operator=(const Grid& rhs) = default;
+    Grid(const Grid &src) = default;
+    Grid &operator=(const Grid &rhs) = default;
 
-    Grid(Grid&& src) = default;
-    Grid& operator=(Grid&& rhs) = default;
+    template <typename E>
+    Grid(const Grid<E> &src);
+    template <typename E>
+    Grid &operator=(const Grid<E> &rhs);
 
-    std::optional<T>& at(size_t x, size_t y);
-    const std::optional<T>& at(size_t x,size_t y) const;
-    
-    size_t getHeight() const {return m_height;}
-    size_t getWidth() const {return m_width;}
+    Grid(Grid &&src) = default;
+    Grid &operator=(Grid &&rhs) = default;
 
-    static const size_t DefaultWidth{10},DefaultHeight{10};
+    std::optional<T> &at(size_t x, size_t y);
+    const std::optional<T> &at(size_t x, size_t y) const;
+
+    size_t getHeight() const { return m_height; }
+    size_t getWidth() const { return m_width; }
+
+    void swap(Grid& other) noexcept;
+
+    static const size_t DefaultWidth{10}, DefaultHeight{10};
 
 private:
     void verifyCoornidate(size_t x, size_t y) const;
 
     std::vector<std::vector<std::optional<T>>> m_cells;
-    size_t m_width{0},m_height{0};
+    size_t m_width{0}, m_height{0};
 };
 template <typename T>
-Grid<T>::Grid(size_t width,size_t height)
-    :m_width{width},m_height{height}
+Grid<T>::Grid(size_t width, size_t height)
+    : m_width{width}, m_height{height}
 {
     m_cells.resize(m_width);
-    for(auto& column:m_cells)
+    for (auto &column : m_cells)
     {
         column.resize(m_height);
     }
 }
 template <typename T>
-void Grid<T>::verifyCoornidate(size_t x,size_t y) const 
+void Grid<T>::verifyCoornidate(size_t x, size_t y) const
 {
-    if(x>=m_width)
+    if (x >= m_width)
     {
-        throw std::out_of_range{std::format("{} must be less than {}.",x,m_width)};
+        throw std::out_of_range{std::format("{} must be less than {}.", x, m_width)};
     }
-    if(y>=m_height)
+    if (y >= m_height)
     {
-        throw std::out_of_range{std::format("{} must be less than {}.",y,m_height)};
+        throw std::out_of_range{std::format("{} must be less than {}.", y, m_height)};
     }
 }
 template <typename T>
-const std::optional<T>& Grid<T>::at(size_t x, size_t y) const
+const std::optional<T> &Grid<T>::at(size_t x, size_t y) const
 {
-    this->verifyCoornidate(x,y);
+    this->verifyCoornidate(x, y);
     return m_cells[x][y];
 }
 template <typename T>
-std::optional<T>& Grid<T>::at(size_t x,size_t y) 
+std::optional<T> &Grid<T>::at(size_t x, size_t y)
 {
-    return const_cast<std::optional<T>&>(std::as_const(*this).at(x,y));
+    return const_cast<std::optional<T> &>(std::as_const(*this).at(x, y));
+}
+template <typename T>
+template <typename E>
+Grid<T>::Grid(const Grid<E> &src)
+    : Grid{src.getWidth(), src.getHeight()}
+{
+    for (size_t i = 0; i < m_width; i++)
+    {
+        for (size_t j = 0; j < m_height; j++)
+        {
+            m_cells[i][j] = src.at(i, j);
+        }
+    }
+}
+template <typename T>
+void Grid<T>::swap(Grid& other) noexcept
+{
+    std::swap(m_width,other.m_width);
+    std::swap(m_height,other.m_height);
+    std::swap(m_cells,other.m_cells);
+}
+template <typename T>
+template <typename E>
+Grid<T>& Grid<T>::operator=(const Grid<E>& rhs)
+{
+    Grid<T> temp{rhs};
+    this->swap(temp);
+    return *this;
 }
 
 template <typename T>
-class SpreadsheetCell 
+class SpreadsheetCell
 {
 public:
-    SpreadsheetCell(T t):m_content{std::move(t)}{}
-    const T& getContent() const {return m_content;}
+    SpreadsheetCell(T t) : m_content{std::move(t)} {}
+    const T &getContent() const { return m_content; }
 
 private:
     T m_content;
 };
-SpreadsheetCell(const char*)->SpreadsheetCell<std::string>;
+SpreadsheetCell(const char *) -> SpreadsheetCell<std::string>;
 void solve()
 {
     std::string myString{"Hello World!"};
