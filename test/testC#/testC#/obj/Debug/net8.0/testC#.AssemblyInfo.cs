10dcf7e1654fed081e4dfb4eaef856e4e6090a03
stwings <571641990@qq.com>
2024-03-28 22:59:22 +0800

    update

--- test/testC#/testC#/obj/Debug/net8.0/testC#.AssemblyInfo.cs
+++ test/testC#/testC#/obj/Debug/net8.0/testC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c312716b270b2b85aa0e56d4e53b5ccde67cf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ff1a8f871aaf897fbc3126508aa83ecc4e230a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("testC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
