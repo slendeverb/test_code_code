1d15c1758544163369f064e346fba7a0a4e41f13
stwings <571641990@qq.com>
2024-03-31 23:58:08 +0800

    update

--- test/testC++/test2.cpp
+++ test/testC++/test2.cpp
@@ -1,4 +1,7 @@
+#include <stdexcept>
+#include <source_location>
 #include <cstdarg>
+#include <filesystem>
 #include <iomanip>
 #include <concepts>
 #include <cstdio>
@@ -27,7 +30,6 @@
 #include <span>
 #include <sstream>
 #include <stack>
-#include <stdexcept>
 #include <string>
 #include <thread>
 #include <unordered_map>
@@ -36,17 +38,101 @@
 #include <vector>
 //
 using namespace std;
+using namespace std::filesystem;
 
-int solve()
+class FileError:public std::exception{
+public:
+    FileError(std::string fileName):m_fileName{std::move(fileName)}{}
+    const char* what() const noexcept override {return m_message.c_str();}
+    virtual const std::string& getFileName() const noexcept {return m_fileName;}
+protected:
+    virtual void setMessage(std::string message){m_message=std::move(message);}
+private:
+    std::string m_fileName;
+    std::string m_message;
+};
+
+class FileOpenError:public FileError{
+public:
+    FileOpenError(std::string fileName):FileError{std::move(fileName)}{
+        setMessage(std::format("Unable to open {}.",getFileName()));
+    }
+protected:
+private:
+};
+
+class FileReadError:public FileError{
+public:
+    FileReadError(std::string fileName,size_t lineNumber)
+        :FileError{std::move(fileName)},m_lineNumber{lineNumber}{
+            setMessage(std::format("Error reading {}, line {}.",getFileName(),m_lineNumber));
+        }
+    virtual const size_t getLineNumber() const noexcept {return m_lineNumber;}
+protected:
+private:
+    size_t m_lineNumber{0};
+};
+
+vector<int> readIntegerFile(string_view filename)
+{
+    ifstream inputStream{filename.data()};
+    if (inputStream.fail())
+    {
+        // We failed to open the file: throw an exception.
+        throw FileOpenError{filename.data()};
+    }
+    // Read the integers one-by-one and add them to a vector.
+    vector<int> integers;
+    size_t  lineNumber{0};
+    while(!inputStream.eof()){
+        std::string line;
+        std::getline(inputStream,line);
+        ++lineNumber;
+        std::istringstream lineStream{line};
+        int temp;
+        while(lineStream>>temp){
+            integers.push_back(temp);
+        }
+        if(!lineStream.eof()){
+            throw FileReadError{filename.data(),lineNumber};
+        }
+    }
+    return integers;
+}
+
+void logMessage(std::string_view message,const std::source_location& location = std::source_location::current()){
+    std::cout<<std::format("{}({}): {}: {}",location.file_name(),location.line(),location.function_name(),message)<<std::endl;
+}
+
+class MyException:public std::exception{
+public:
+    MyException(std::string message,std::source_location location=std::source_location::current())
+        :m_message{std::move(message)},m_location{std::move(location)}{}
+    const char* what() const noexcept override {return m_message.c_str();}
+    virtual const std::source_location& where() const noexcept {return m_location;}
+protected:
+private:
+    std::string m_message;
+    std::source_location m_location;
+};
+
+
+
+void solve()
 {
     
 }
 
 int main()
 {
-    freopen("../in.txt", "r", stdin);
-    freopen("../out.txt", "w", stdout);
-    freopen("../err.txt", "w", stderr);
+    std::ifstream in{"../in.txt"};
+    std::streambuf *oldIn{std::cin.rdbuf(in.rdbuf())};
+    std::ofstream out{"../out.txt"};
+    std::streambuf *oldOut{std::cout.rdbuf(out.rdbuf())};
+    std::ofstream err{"../err.txt"};
+    std::streambuf *oldErr{std::cerr.rdbuf(err.rdbuf())};
+    std::streambuf *oldLog{std::clog.rdbuf(err.rdbuf())};
+
     clock_t startTime{clock()};
     solve();
     clock_t endTime{clock()};
