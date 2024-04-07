60b4e09cef8421c3e03bacb29234ddc84cdac8ed
stwings <571641990@qq.com>
2024-04-07 21:38:54 +0800

    update

--- test/testC++/test/src/main.cpp
+++ test/testC++/test/src/main.cpp
@@ -22,6 +22,7 @@
 #include <ctime>
 #include <deque>
 #include <exception>
+#include <execution>
 #include <filesystem>
 #include <format>
 #include <forward_list>
@@ -70,121 +71,10 @@
 //
 using namespace std;
 
-double arithmeticMean(std::span<const int> values) {
-    double sum{std::accumulate(std::cbegin(values), std::cend(values), 0.0)};
-    return sum / values.size();
-}
 
-double geometricMean(std::span<const int> values) {
-    // int mult{std::accumulate(std::cbegin(values),std::cend(values),1,[](int
-    // value1,int value2){return value1*value2;})};
-    int mult{std::accumulate(std::cbegin(values), std::cend(values), 1,
-                             std::multiplies<>{})};
-    return pow(mult, 1.0 / values.size());
-}
-
-template <typename Container>
-void populateContainer(Container& cont) {
-    while (true) {
-        cout << "Enter a number (0 to quit): \n";
-        int value;
-        cin >> value;
-        if (value == 0) {
-            break;
-        }
-        cont.push_back(value);
-    }
-}
-
-class MyClass {
-   public:
-    MyClass() = default;
-    MyClass(const MyClass& src) = default;
-    MyClass(string str) : m_str{move(str)} {}
-    virtual ~MyClass() = default;
-    // Move assignment operator
-    MyClass& operator=(MyClass&& rhs) noexcept {
-        if (this == &rhs) {
-            return *this;
-        }
-        m_str = move(rhs.m_str);
-        cout << format("Move operator= (m_str={})", m_str) << endl;
-        return *this;
-    }
-    void setString(string str) { m_str = move(str); }
-    const string& getString() const { return m_str; }
-
-   private:
-    string m_str;
-};
-
-void removeEmptyStrings(std::vector<std::string>& strings) {
-    std::erase_if(strings, [](const std::string& str) { return str.empty(); });
-}
-
-void removeEmptyStringsWithoutAlgorithms(std::vector<std::string>& strings) {
-    for (auto it{std::begin(strings)}; it != std::end(strings);) {
-        if (it->empty()) {
-            it = strings.erase(it);
-        } else {
-            ++it;
-        }
-    }
-}
-
-void removeEmptyStringsWithRemoveEraseIdiom(std::vector<std::string>& strings) {
-    auto itToBeErased{
-        std::remove_if(std::begin(strings), std::end(strings),
-                       [](const std::string& str) { return str.empty(); })};
-    strings.erase(itToBeErased, std::end(strings));
-}
-
-void printContainer(const auto& container) {
-    for (auto& x : container) {
-        std::cout << x << " ";
-    }
-    std::cout << std::endl;
-}
-
-class SumAndProduct {
-   public:
-    void operator()(int value) {
-        m_sum += value;
-        m_product *= value;
-    }
-    int getSum() const { return m_sum; }
-    int getProduct() const { return m_product; }
-
-   private:
-    int m_sum{0};
-    int m_product{1};
-};
 
 void solve() {
-    vector<int> values;
-    populateContainer(values);
-    // Sort the container
-    sort(begin(values), end(values));
-    cout << "Sorted vector: ";
-    for (const auto& i : values) {
-        cout << i << " ";
-    }
-    cout << endl;
-    while (true) {
-        int number;
-        cout << "Enter a number to insert (0 to quit): ";
-        cin >> number;
-        if (number == 0) {
-            break;
-        }
-        auto iter{lower_bound(begin(values), end(values), number)};
-        values.insert(iter, number);
-        cout << "New vector: ";
-        for (const auto& i : values) {
-            cout << i << " ";
-        }
-        cout << endl;
-    }
+    
 }
 
 int main() {
