74552e334ee893c819152903badc2e9fc00da1c1
stwings <571641990@qq.com>
2024-05-04 16:40:12 +0800

    update

--- test/testRust/test_rust/src/main.rs
+++ test/testRust/test_rust/src/main.rs
@@ -1,12 +1,20 @@
-use std::collections::HashSet;
 
-fn simple(i:&[u8])->usize{
-    return i.windows(14)
-        .position(|w|{
-            return w.iter().collect::<HashSet<_>>().len()==14;
-        })
-        .map(|x| x+14)
-        .unwrap();
+fn simple(input:&[u8])->Option<usize>{
+    let mut idx=0;
+    while let Some(slice)=input.get(idx..idx+14){
+        let mut state=0u32;
+        if let Some(pos) = slice.iter().rposition(|byte|{
+            let bit_idx=byte%32;
+            let ret=state&(1<<bit_idx)!=0;
+            state|=1<<bit_idx;
+            ret
+        }) {
+            idx+=pos+1;
+        }else {
+            return Some(idx);
+        }
+    }
+    return None;
 }
 
 fn main() {
