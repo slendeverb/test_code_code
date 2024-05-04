276ceeed18a34305c31d9b88b3073ece7ad79798
stwings <571641990@qq.com>
2024-05-04 16:42:27 +0800

    update

--- test/testRust/test_rust/src/main.rs
+++ test/testRust/test_rust/src/main.rs
@@ -1,22 +1,19 @@
-
-fn simple(input:&[u8])->Option<usize>{
-    let mut idx=0;
-    while let Some(slice)=input.get(idx..idx+14){
-        let mut state=0u32;
-        if let Some(pos) = slice.iter().rposition(|byte|{
-            let bit_idx=byte%32;
-            let ret=state&(1<<bit_idx)!=0;
-            state|=1<<bit_idx;
+fn simple(input: &[u8]) -> Option<usize> {
+    let mut idx = 0;
+    while let Some(slice) = input.get(idx..idx + 14) {
+        let mut state = 0u32;
+        if let Some(pos) = slice.iter().rposition(|byte| {
+            let bit_idx = byte % 32;
+            let ret = state & (1 << bit_idx) != 0;
+            state |= 1 << bit_idx;
             ret
         }) {
-            idx+=pos+1;
-        }else {
+            idx += pos + 1;
+        } else {
             return Some(idx);
         }
     }
     return None;
 }
 
-fn main() {
-    
-}
+fn main() {}
