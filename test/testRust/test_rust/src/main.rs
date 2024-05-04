76ffb667ebdbc46eeeb9ff4cc985784f152bb960
stwings <571641990@qq.com>
2024-05-04 16:10:07 +0800

    update

--- test/testRust/test_rust/src/main.rs
+++ test/testRust/test_rust/src/main.rs
@@ -1,3 +1,14 @@
+use std::collections::HashSet;
+
+fn simple(i:&[u8])->usize{
+    return i.windows(14)
+        .position(|w|{
+            return w.iter().collect::<HashSet<_>>().len()==14;
+        })
+        .map(|x| x+14)
+        .unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    
 }
