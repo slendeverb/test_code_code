9210f429647873430a77441694101e953db9c13e
stwings <571641990@qq.com>
2024-05-13 12:29:29 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -1,28 +1,8 @@
-#include <filesystem>
-#include <fstream>
 #include <iostream>
-#include <string>
-#include <vector>
 
 void solve()
 {
-    std::string output_path { "../../../out.txt" };
-    std::ofstream out { output_path };
-    std::filesystem::path folder_path { "C:/Users/slendeverb/Documents/code_code" };
-    if (!std::filesystem::exists(folder_path)) {
-        out << "folder path doesn't exist" << std::endl;
-    }
-    std::filesystem::directory_entry entry { folder_path };
-    if (entry.status().type() == std::filesystem::file_type::directory) {
-        out << "This root is a directory\n";
-    }
-    std::filesystem::recursive_directory_iterator list { folder_path };
-    for (auto& it : list) {
-        if (!it.is_directory()) {
-            out << it.path() << "\n";
-        }
-    }
-    out.close();
+    
 }
 
 int main()
--- test/testJava/Test2/Test2/src/App.java
+++ test/testJava/Test2/Test2/src/App.java
@@ -1,27 +1,8 @@
-import java.io.*;
-import java.util.regex.*;
+
 
 public class App {
     public static void main(String[] args) {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("test\\testJava\\Test2\\Test2\\src\\App.java"));
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                parse(line);
-            }
-            br.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        
     }
 
-    private static void parse(String line) {
-        Pattern p = Pattern.compile("\"[\\w[ -~`!@#$%^&*()=+{}:;''<>,.?/]]*\"");
-        Matcher m = p.matcher(line);
-        while (m.find()) {
-            System.out.println(m.group());
-        }
-    }
 }
