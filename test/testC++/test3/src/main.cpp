b09c679bd23be28c1c90fbefb4c33e5c88947fb3
stwings <571641990@qq.com>
2024-05-17 22:02:28 +0800

    update

--- test/testC++/test3/src/main.cpp
+++ test/testC++/test3/src/main.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 #include <print>
-#include <atomic>
-#include <thread>
-
-void foo1(int i){
-    std::println("{} {}","I love progarmming.",i);
-}
 
 void solve() {
-    std::thread t1{foo1,0};
     
-    t1.join();
 }
 
 int main() {
