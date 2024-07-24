7189e79a544d449acc9ec240872e2ee9164eb1fc
stwings <571641990@qq.com>
2024-07-24 10:28:28 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
 
 void solve() {
-    const int x=3019226;
-    std::cout<<std::hex<<x<<"\n";
-    std::cout<<std::oct<<x<<"\n";
-    std::cout<<std::bitset<22>(x)<<"\n";
-    std::cout<<"\u221e\n";
+    
 }
 
 int main() {
