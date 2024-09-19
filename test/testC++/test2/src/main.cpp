0052d597f4fbf6e6e3a16071f853e2306971d513
stwings <571641990@qq.com>
2024-09-19 12:49:51 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -3,33 +3,27 @@
 
 class Solution{
 public:
-    int latestTimeCatchTheBus(std::vector<int>& buses, std::vector<int>& passengers, int capacity){
-        std::ranges::sort(buses);
-        std::ranges::sort(passengers);
-        int pos=0,space=0;
-        size_t passengers_num=passengers.size();
-        for(int arrive_time:buses){
-            space=capacity;
-            while(space>0&&pos<passengers_num&&passengers[pos]<=arrive_time){
-                space--;
-                pos++;
-            }
+    int longestContinuousSubstring(std::string s){
+        size_t size=s.size();
+        if(size==1){
+            return 1;
         }
-
-        pos--;
-        int ans=space>0?buses.back():passengers[pos];
-        while(pos>=0&&passengers[pos]==ans){
-            pos--;
-            ans--;
+        int ans=1,temp=1;
+        for(int i=1;i<size;i++){
+            if(s[i]==s[i-1]+1){
+                temp++;
+            }
+            else{
+                temp=1;
+            }
+            ans=std::max(ans,temp);
         }
         return ans;
     }
 };
 
 int main(int argc, char** argv) {
-    std::vector<int> buses{10,20};
-    std::vector<int> passengers{2,17,18,19};
-    const int capacity=2;
+    std::string s="abcde";
     Solution so;
-    std::cout<<"The latest time to catch the bus is: "<<so.latestTimeCatchTheBus(buses,passengers,capacity)<<std::endl;
+    std::cout<<so.longestContinuousSubstring(s);
 }
