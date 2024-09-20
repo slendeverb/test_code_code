8ab7a94999894d66dd36a37bc3dfb6c27250bb16
stwings <571641990@qq.com>
2024-09-20 17:36:11 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -2,29 +2,57 @@
 
 class Solution {
    public:
-    int countSpecialNumbers(int n) {
-        std::string s = std::to_string(n);
-        auto dp = [f = [&s](auto&& self, int i, int mask, bool is_limit, bool is_num) -> int {
-            if (i == s.size()) {
+    int countSpecialNumbers(int n) { return numberOfPowerfulInt(1, n, 9, ""); }
+
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, std::string s) {
+        std::string high = std::to_string(finish);
+        long long n = high.size();
+        std::string low = std::to_string(start);
+        std::string zeros(n - low.size(), '0');
+        low = zeros + low;
+        long long diff = n - s.size();
+        std::vector<std::vector<long long>> memo(n, std::vector<long long>(1 << 10, -1));
+        auto dp = [f = [&](auto&& self, long long i, long long mask, bool limit_low,
+                           bool limit_high, bool is_num) -> long long {
+            if (i == n) {
                 return is_num;
             }
-            int res = 0;
-            if (!is_num) {
-                res = self(self, i + 1, mask, false, false);
-            }
-            int up = is_limit ? s[i] - '0' : 9;
-            for (int d = (1 - is_num); d <= up; d++) {
-                if (((mask >> d) & 1) == 0) {
-                    res += self(self, i + 1, mask | (1 << d), is_limit && d == up, true);
+            long long res = 0;
+            if (!is_num && low[i] == '0') {
+                if (i < diff) {
+                    res = self(self, i + 1, mask, true, false, false);
                 }
             }
+            if (!limit_low && !limit_high && memo[i][mask] != -1) {
+                return memo[i][mask];
+            }
+            int lo = limit_low ? (low[i] - '0') : 0;
+            int hi = limit_high ? (high[i] - '0') : 9;
+            int d0 = is_num ? 0 : 1;
+            if (i < diff) {
+                for (int d = std::max(lo, d0); d <= std::min(hi, limit); d++) {
+                    // if (((mask >> d) & 1) == 0)
+                        res += self(self, i + 1, mask | (1 << d), limit_low && d == lo,
+                                    limit_high && d == hi, true);
+                }
+            } else {
+                int x = s[i - diff] - '0';
+                if (x >= std::max(lo, d0) && x <= std::min(hi, limit)) {
+                    res = self(self, i + 1, mask | (1 << x), limit_low && x == lo,
+                               limit_high && x == hi, true);
+                }
+            }
+            if (!limit_low && !limit_high) {
+                memo[i][mask] = res;
+            }
             return res;
-        }](int i, int mask, bool is_limit, bool is_num) { return f(f, i, mask, is_limit, is_num); };
-        return dp(0, 0, true, false);
+        }](long long i, long long mask, bool limit_low, bool limit_high, bool is_num) {
+            return f(f, i, mask, limit_low, limit_high, is_num);
+        };
+        return dp(0, 0, true, true, false);
     }
 };
 
 int main(int argc, char** argv) {
-    int n = 135;
-    std::cout << std::format("{}\n", Solution{}.countSpecialNumbers(n));
+    std::cout << std::format("{}\n", Solution{}.countSpecialNumbers(174017551));
 }
