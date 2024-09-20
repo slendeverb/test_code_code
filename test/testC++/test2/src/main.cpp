ffd28d4504365b516e6f983d8c18b2a84f5103ac
stwings <571641990@qq.com>
2024-09-20 14:46:56 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -4,26 +4,27 @@ class Solution {
    public:
     int countSpecialNumbers(int n) {
         std::string s = std::to_string(n);
-        auto f = [&s](this auto&& self, int i, int mask, bool is_limit, bool is_num) -> int {
+        auto dp = [f = [&s](auto&& self, int i, int mask, bool is_limit, bool is_num) -> int {
             if (i == s.size()) {
                 return is_num;
             }
             int res = 0;
             if (!is_num) {
-                res = self(i + 1, mask, false, false);
+                res = self(self, i + 1, mask, false, false);
             }
             int up = is_limit ? s[i] - '0' : 9;
             for (int d = (1 - is_num); d <= up; d++) {
                 if (((mask >> d) & 1) == 0) {
-                    res += self(i + 1, mask |= (1 << d), is_limit && d == up, true);
+                    res += self(self, i + 1, mask | (1 << d), is_limit && d == up, true);
                 }
             }
             return res;
-        };
-        return f(0, 0, true, false);
+        }](int i, int mask, bool is_limit, bool is_num) { return f(f, i, mask, is_limit, is_num); };
+        return dp(0, 0, true, false);
     }
 };
 
 int main(int argc, char** argv) {
-    
+    int n = 135;
+    std::cout << std::format("{}\n", Solution{}.countSpecialNumbers(n));
 }
