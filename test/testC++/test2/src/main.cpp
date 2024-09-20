6e449f65107485b141df28cc5c6e125bd9ad5248
stwings <571641990@qq.com>
2024-09-20 14:21:29 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -1,29 +1,5 @@
-// #include "header.h"
-#include <bits/stdc++.h>
-
-class Solution{
-public:
-    int longestContinuousSubstring(std::string s){
-        size_t size=s.size();
-        if(size==1){
-            return 1;
-        }
-        int ans=1,temp=1;
-        for(int i=1;i<size;i++){
-            if(s[i]==s[i-1]+1){
-                temp++;
-            }
-            else{
-                temp=1;
-            }
-            ans=std::max(ans,temp);
-        }
-        return ans;
-    }
-};
+#include "header.h"
 
 int main(int argc, char** argv) {
-    std::string s="abcdefgijk";
-    Solution so;
-    std::cout<<so.longestContinuousSubstring(s);
+    
 }
