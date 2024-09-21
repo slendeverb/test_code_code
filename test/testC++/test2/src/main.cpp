86ab0c217eb2722fe5fd0338f55224ff274a3635
stwings <571641990@qq.com>
2024-09-22 01:08:29 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -1,27 +1,89 @@
 #include "header.h"
 
-class Solution {
-   public:
-    int edgeScore(std::vector<int>& edges){
-        int n=edges.size();
-        std::vector<long long> score(n,0);
-        for(int i=0;i<n;i++){
-            score[edges[i]]+=i;
+class Expression{
+public:
+    virtual int interpreter(std::map<char,int>& var)=0;
+    virtual ~Expression(){}
+};
+
+class VarExpression:public Expression{
+public:
+    VarExpression(const char& key):key{key}{}
+    virtual int interpreter(std::map<char,int>& var) override{
+        return var[key];
+    }
+private:
+    char key;
+};
+
+class SymbolExpression:public Expression{
+public:
+    SymbolExpression(Expression* left, Expression* right):left{left},right{right}{}
+    virtual ~SymbolExpression(){
+        if(left!=nullptr){
+            delete left;
         }
-        int ans=0;
-        long long max=0;
-        for(int i=0;i<n;i++){
-            if(score[i]>max){
-                max=score[i];
-                ans=i;
-            }
+        if(right!=nullptr){
+            delete right;
         }
-        return ans;
+    }
+protected:
+    Expression* left;
+    Expression* right;
+};
+
+class AddExpression:public SymbolExpression{
+public:
+    AddExpression(Expression* left,Expression* right):SymbolExpression(left,right){}
+    virtual int interpreter(std::map<char,int>& var) override{
+        return left->interpreter(var)+right->interpreter(var);
     }
 };
 
+class SubExpression:public SymbolExpression{
+public:
+    SubExpression(Expression* left, Expression* right):SymbolExpression(left,right){}
+    virtual int interpreter(std::map<char,int>& var) override{
+        return left->interpreter(var)-right->interpreter(var);
+    }
+};
+
+Expression* analyse(std::string& expStr){
+    std::stack<Expression*> expStack;
+    Expression* left=nullptr;
+    Expression* right=nullptr;
+    for(int i=0;i<expStr.size();++i){
+        switch(expStr[i]){
+            case '+':
+                left=expStack.top();
+                right=new VarExpression(expStr[++i]);
+                expStack.push(new AddExpression(left,right));
+                break;
+            case '-':
+                left=expStack.top();
+                right=new VarExpression(expStr[++i]);
+                expStack.push(new SubExpression(left,right));
+                break;
+            default:
+                expStack.push(new VarExpression(expStr[i]));
+        }
+    }
+    Expression* expression=expStack.top();
+    return expression;
+}
+
+void release(Expression* expression){
+    if(expression!=nullptr){
+        delete expression;
+        expression=nullptr;
+    }
+}
+
 int main(int argc, char** argv) {
-    std::vector<int> nums{1,0,0,0,0,7,7,5};
-    auto ans = Solution{}.edgeScore(nums);
-    std::println("{}",ans);
+    std::string expStr="a+b-c+d";
+    std::map<char,int> var{{'a',5},{'b',2},{'c',1},{'d',6}};
+    Expression* expression=analyse(expStr);
+    int res=expression->interpreter(var);
+    std::println("{}",res);
+    release(expression);
 }
