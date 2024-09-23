df0fefbc534ee9934e38ab8d3268575cf0f0a219
stwings <571641990@qq.com>
2024-09-23 15:57:57 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -2,12 +2,12 @@
 
 class Solution {
    public:
-    int maxScoreSightseeingPair(std::vector<int>& values){
-        int ans=0;
-        int mx=values[0];
-        for(int j=1;j<values.size();j++){
-            ans=std::max(ans,mx+values[j]-j);
-            mx=std::max(mx,values[j]+j);
+    int maxScoreSightseeingPair(std::vector<int>& values) {
+        int ans = 0;
+        int mx = values[0];
+        for (int j = 1; j < values.size(); j++) {
+            ans = std::max(ans, mx + values[j] - j);
+            mx = std::max(mx, values[j] + j);
         }
         return ans;
     }
@@ -17,12 +17,12 @@ int main(int argc, char** argv) {
     std::vector<int> values;
     std::ifstream input{"../../../in.txt"};
     std::string s;
-    while(!std::getline(input,s,',').eof()){
+    while (!std::getline(input, s, ',').eof()) {
         values.push_back(std::stoi(s));
     }
-    auto start=std::chrono::steady_clock::now();
-    auto ans=Solution{}.maxScoreSightseeingPair(values);
-    auto end=std::chrono::steady_clock::now();
-    auto elapsed_seconds=std::chrono::duration<int64_t,std::nano>(end-start);
-    std::println("ans: {}, time: {}",ans, elapsed_seconds);
+    auto start = std::chrono::steady_clock::now();
+    auto ans = Solution{}.maxScoreSightseeingPair(values);
+    auto end = std::chrono::steady_clock::now();
+    auto elapsed_seconds = std::chrono::duration<int64_t, std::nano>(end - start);
+    std::println("ans: {}, time: {}", ans, elapsed_seconds);
 }
