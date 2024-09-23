300e263757e816ffdbdadccf90969c2d589315d6
stwings <571641990@qq.com>
2024-09-23 22:58:01 +0800

    update

--- test/testRust/test_rust2/src/main.rs
+++ test/testRust/test_rust2/src/main.rs
@@ -1,30 +1,38 @@
-use std::{fs, time::Instant};
+use std::iter::zip;
 
-fn main() {
-    let mut values = Vec::new();
-    let path = "../../in.txt";
-    for x in fs::read_to_string(path)
-        .expect("Can not open the file")
-        .split(',')
-    {
-        values.push(x.parse::<i32>().unwrap());
-    }
-    let now = Instant::now();
-    let ans = Solution::max_score_sightseeing_pair(values);
-    let elapsed_time = now.elapsed();
-    println!("ans: {}, time: {} ns", ans, elapsed_time.as_nanos());
+#[derive(Debug)]
+struct Counter {
+    count: u32,
 }
 
-struct Solution {}
-
-impl Solution {
-    pub fn max_score_sightseeing_pair(values: Vec<i32>) -> i32 {
-        let mut ans = 0i32;
-        let mut mx = 0;
-        for (j, val) in values.iter().enumerate() {
-            ans = ans.max(mx + val - j as i32);
-            mx = mx.max(val + j as i32);
-        }
-        ans
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
     }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+fn iter_methods() {
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    println!("{}", sum);
+}
+
+fn main() {
+    iter_methods();
 }
