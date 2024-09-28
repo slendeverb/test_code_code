21c27622139ce3e2feb5bf1cabca203ccc365867
stwings <571641990@qq.com>
2024-09-28 14:28:11 +0800

    update

--- test/testC++/test3/include/header.h
+++ test/testC++/test3/include/header.h
@@ -1,26 +1,8 @@
 #pragma once
-#include <algorithm>
-#include <cmath>
-#include <cstring>
-#include <deque>
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <locale>
-#include <map>
-#include <numeric>
-#include <print>
-#include <queue>
-#include <ranges>
-#include <set>
-#include <string>
-#include <thread>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
+#include <bits/stdc++.h>
 #define NOMINMAX
 #include <winsock2.h>
 #pragma comment(lib, "Ws2_32.lib")
 
-// #include <opencv2/opencv.hpp>
-// #include <torch/torch.h>
+#include <opencv2/opencv.hpp>
+#include <torch/torch.h>
--- test/testC++/test3/src/main.cpp
+++ test/testC++/test3/src/main.cpp
@@ -1,18 +1,34 @@
-// #include "header.h"
+#include "header.h"
 #include <bits/stdc++.h>
 
-void delete_point(const std::string& s){
-    std::string ans;
-    for(const auto c:s){
-        if(c!='.'){
-            ans.push_back(c);
-        }
+class BookMyShow{
+public:
+    BookMyShow(int n,int m){
+
     }
-    std::cout<<ans<<"\n";
-}
+
+    std::vector<int> gather(int k,int maxRow){
+
+    }
+
+    bool scatter(int k,int maxRow){
+
+    }
+};
 
 int main(int argc,char** argv){
-    std::string s;
-    std::cin>>s;
-    delete_point(s);
+    cv::Mat img=cv::imread("C:\\Users\\slendeverb\\Pictures\\Saved Pictures\\117541346_p0_master1200.jpg");
+    cv::imshow("show",img);
+    cv::waitKey(3000);
+
+    torch::Tensor tensor=torch::rand({10,10});
+    if(torch::cuda::is_available()){
+        std::cout<<"CUDA is available! Training on GPU!\n";
+        auto tensor_cuda=tensor.cuda();
+        std::cout<<tensor_cuda<<"\n";
+    }
+    else{
+        std::cout<<"CUDA is not available! Traning on CPU!\n";
+        std::cout<<tensor<<"\n";
+    }
 }
