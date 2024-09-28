7da1422ed7307225b96310d75c6b2ee6930a7bd4
stwings <571641990@qq.com>
2024-09-28 16:46:49 +0800

    update

--- test/testC++/test3/include/header.h
+++ test/testC++/test3/include/header.h
@@ -4,5 +4,5 @@
 #include <winsock2.h>
 #pragma comment(lib, "Ws2_32.lib")
 
-#include <opencv2/opencv.hpp>
-#include <torch/torch.h>
+// #include <opencv2/opencv.hpp>
+// #include <torch/torch.h>
--- test/testC++/test3/src/main.cpp
+++ test/testC++/test3/src/main.cpp
@@ -17,18 +17,5 @@ public:
 };
 
 int main(int argc,char** argv){
-    cv::Mat img=cv::imread("C:\\Users\\slendeverb\\Pictures\\Saved Pictures\\117541346_p0_master1200.jpg");
-    cv::imshow("show",img);
-    cv::waitKey(3000);
-
-    torch::Tensor tensor=torch::rand({10,10});
-    if(torch::cuda::is_available()){
-        std::cout<<"CUDA is available! Training on GPU!\n";
-        auto tensor_cuda=tensor.cuda();
-        std::cout<<tensor_cuda<<"\n";
-    }
-    else{
-        std::cout<<"CUDA is not available! Traning on CPU!\n";
-        std::cout<<tensor<<"\n";
-    }
+    
 }
