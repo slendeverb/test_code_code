40f7407e31d59453a681d6e3427b8e5005e1c01b
stwings <571641990@qq.com>
2024-09-28 19:21:48 +0800

    update

--- test/testC++/test3/src/main.cpp
+++ test/testC++/test3/src/main.cpp
@@ -1,20 +1,4 @@
 #include "header.h"
-#include <bits/stdc++.h>
-
-class BookMyShow{
-public:
-    BookMyShow(int n,int m){
-
-    }
-
-    std::vector<int> gather(int k,int maxRow){
-
-    }
-
-    bool scatter(int k,int maxRow){
-
-    }
-};
 
 int main(int argc,char** argv){
     
