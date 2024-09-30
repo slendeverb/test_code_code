41cfc5afe9066f34c3b1c06f60fe9a2298cd1762
stwings <571641990@qq.com>
2024-09-30 16:03:33 +0800

    update

--- test/testC++/test3/src/main.cpp
+++ test/testC++/test3/src/main.cpp
@@ -1,20 +1,28 @@
 // #include "header.h"
 #include <bits/stdc++.h>
 
-class Solution{
-public:
-    int timeRequiredToBuy(std::vector<int>& tickets,int k){
-        int res=0;
-        int tk=tickets[k];
-        for(int i=0;i<tickets.size();i++){
-            res+=std::min(tickets[i],tk-(i>k));
+class SeatManager{
+    public:
+        SeatManager(int n){
+            for(int i=1;i<=n;i++){
+                pq.emplace(i);
+            }
         }
-        return res;
-    }
+        
+        int reserve(){
+            int res=pq.top();
+            pq.pop();
+            return res;
+        }
+        
+        void unreserve(int seatNumber){
+            pq.push(seatNumber);
+        }
+    
+    private:
+        std::priority_queue<int,std::vector<int>,std::greater<>> pq;
 };
 
-int main(int argc, char** argv) {
-    std::vector<int> tickets{5,1,1,1};
-    int k=0;
-    std::cout<<Solution{}.timeRequiredToBuy(tickets,k)<<"\n";
+int main(int argc,char** argv){
+    
 }
--- test/testJava/test/src/main/java/org/test/App.java
+++ test/testJava/test/src/main/java/org/test/App.java
@@ -1,8 +1,30 @@
 package org.test;
 
-
 public class App {
     public static void main(String[] args) {
+        MyClass myClass1=MyClass.getInstance();
+        MyClass myClass2=MyClass.getInstance();
+        System.out.println(myClass1);
+        System.out.println(myClass2);
         
     }
+}
+
+class MyClass{
+    private MyClass(){
+
+    }
+
+    private MyClass(MyClass myClass){
+
+    }
+
+    public static MyClass getInstance(){
+        if(myClass==null){
+            myClass=new MyClass();
+        }
+        return myClass;
+    }
+
+    private static volatile MyClass myClass;
 }
