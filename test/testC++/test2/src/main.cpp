33774bf12517c6e0e86158dd6516192b69534738
stwings <571641990@qq.com>
2024-10-08 08:48:26 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -3,33 +3,26 @@
 
 class Solution {
    public:
-    int minRefuelStops(int target,int startFuel,std::vector<std::vector<int>>& stations){
-        stations.push_back({target,0});
-        int ans=0;
-        int pre_position=0;
-        int cur_fuel=startFuel;
-        std::priority_queue<int> fuel_heap;
-        for(const auto& station:stations){
-            int position=station[0];
-            cur_fuel-=position-pre_position;
-            while (!fuel_heap.empty()&&cur_fuel<0){
-                cur_fuel+=fuel_heap.top();
-                fuel_heap.pop();
-                ans++;
+    std::string destCity(std::vector<std::vector<std::string>>& paths) {
+        std::map<std::string, bool> dest;
+        std::string res;
+        for (const auto& path : paths) {
+            dest[path[0]] = false;
+            if(!dest.contains(path[1])){
+                dest[path[1]]=true;
             }
-            if(cur_fuel<0){
-                return -1;
-            }
-            fuel_heap.push(station[1]);
-            pre_position=position;
         }
-        return ans;
+        for (const auto& x : dest) {
+            if (x.second == true) {
+                res = x.first;
+            }
+        }
+        return res;
     }
 };
 
 int main(int argc, char** argv) {
-    int target{100};
-    int startFuel{10};
-    std::vector<std::vector<int>> stations{{10,60},{20,30},{30,30},{60,40}};
-    std::cout<<std::format("{}\n",Solution{}.minRefuelStops(target,startFuel,stations));
+    std::vector<std::vector<std::string>> paths{
+        {"London", "New York"}, {"New York", "Lima"}, {"Lima", "Sao Paulo"}};
+    std::cout<<std::format("{}\n",Solution{}.destCity(paths));
 }
