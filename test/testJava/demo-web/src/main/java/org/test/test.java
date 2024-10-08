03a9cb51577f92f71ef2237b872e8a4055f21316
stwings <571641990@qq.com>
2024-10-08 17:18:05 +0800

    update

--- test/testJava/demo-web/src/main/java/org/test/test.java
+++ test/testJava/demo-web/src/main/java/org/test/test.java
@@ -2,6 +2,11 @@ package org.test;
 
 public class test {
     public static void main(String[] args) {
-        
+        int num = 7 + (int) (Math.random() * 13);
+        if (num <= 13) {
+            System.out.print("<h1>Hello</h1><br>");
+        } else {
+            System.out.print("<h1>hello</h1><br>");
+        }
     }
 }
