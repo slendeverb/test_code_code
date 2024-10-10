049d4b0697d31d07cad32d765c53207f43c78ae4
stwings <571641990@qq.com>
2024-10-10 16:25:48 +0800

    update

--- template/template/algorithm/prime/Eratosthenes.cpp
+++ template/template/algorithm/prime/Eratosthenes.cpp
@@ -7,18 +7,16 @@ std::vector<int> prime;
 std::bitset<N> is_prime;
 
 void Eratosthenes(int n) {
+    is_prime.set();
     is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i <= n; ++i)
-        is_prime[i] = true;
-    // i * i <= n 说明 i <= sqrt(n)
-    for (int i = 2; i * i <= n; ++i) {
+    for (int i = 2; i * i <= n; i++) {
         if (is_prime[i]) {
             for (int j = i * i; j <= n; j += i) {
                 is_prime[j] = false;
             }
         }
     }
-    for (int i = 2; i <= n; ++i) {
+    for (int i = 2; i <= n; i++) {
         if (is_prime[i]) {
             prime.push_back(i);
         }
--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -2,26 +2,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-   public:
-    int distinctPrimeFactors(vector<int>& nums) {
-        std::unordered_set<int> s;
-        for (const auto& num : nums) {
-            auto x{num};
-            for (int i = 2; i * i <= num; i++) {
-                if (x % i == 0) {
-                    s.insert(i);
-                    while (x % i == 0) {
-                        x /= i;
-                    }
-                }
-            }
-            if (x > 1) {
-                s.insert(x);
+static const size_t N = 1e6 + 1;
+std::vector<int> prime;
+std::bitset<N> is_prime;
+
+int Eratosthenes = [](int n) {
+    is_prime.set();
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                is_prime[j] = false;
             }
         }
-        return s.size();
+    }
+    for (int i = 2; i <= n; i++) {
+        if (is_prime[i]) {
+            prime.push_back(i);
+        }
+    }
+    prime.emplace_back(N);
+    prime.emplace_back(N);
+    return 0;
+}(N);
+
+class Solution {
+   public:
+    vector<int> closestPrimes(int left, int right) {
+        int p = -1, q = -1;
+        auto i = std::lower_bound(prime.begin(), prime.end(), left) - prime.begin();
+        for (; prime[i + 1] <= right; i++) {
+            if (p < 0 || prime[i + 1] - prime[i] < q - p) {
+                p = prime[i];
+                q = prime[i + 1];
+            }
+        }
+        return {p, q};
     }
 };
 
-int main(int argc, char** argv) {}
+int main(int argc, char** argv) {
+    int left{19};
+    int right{31};
+    const auto ans{Solution{}.closestPrimes(left, right)};
+    for (const auto& x : ans) {
+        std::cout << std::format("{} ", x);
+    }
+    std::cout << std::format("\n");
+}
