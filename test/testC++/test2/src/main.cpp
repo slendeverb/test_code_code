cfe15f404daeb914258fb2d09f5940bb0a3a6360
stwings <571641990@qq.com>
2024-10-11 17:20:03 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -2,51 +2,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-static const size_t N = 1e6 + 1;
-std::vector<int> prime;
-std::bitset<N> is_prime;
-
-int Eratosthenes = [](int n) {
-    is_prime.set();
-    is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i * i <= n; i++) {
-        if (is_prime[i]) {
-            for (int j = i * i; j <= n; j += i) {
-                is_prime[j] = false;
-            }
-        }
-    }
-    for (int i = 2; i <= n; i++) {
-        if (is_prime[i]) {
-            prime.push_back(i);
-        }
-    }
-    prime.emplace_back(N);
-    prime.emplace_back(N);
-    return 0;
-}(N);
-
 class Solution {
-   public:
-    vector<int> closestPrimes(int left, int right) {
-        int p = -1, q = -1;
-        auto i = std::lower_bound(prime.begin(), prime.end(), left) - prime.begin();
-        for (; prime[i + 1] <= right; i++) {
-            if (p < 0 || prime[i + 1] - prime[i] < q - p) {
-                p = prime[i];
-                q = prime[i + 1];
+public:
+    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        long long ans=0;
+        std::unordered_map<int,int> cnt;
+        for(int x:nums1){
+            if(x%k){
+                continue;
+            }
+            x/=k;
+            for(int d=1;d*d<=x;d++){
+                if(x%d){
+                    continue;
+                }
+                cnt[d]++;
+                if(d*d<x){
+                    cnt[x/d]++;
+                }
             }
         }
-        return {p, q};
+        for(int x:nums2){
+            ans+=cnt.contains(x)?cnt[x]:0;
+        }
+        return ans;
     }
 };
 
 int main(int argc, char** argv) {
-    int left{19};
-    int right{31};
-    const auto ans{Solution{}.closestPrimes(left, right)};
-    for (const auto& x : ans) {
-        std::cout << std::format("{} ", x);
-    }
-    std::cout << std::format("\n");
+    std::vector<int> nums1{};
+    std::vector<int> nums2{};
+    int k{};
+    std::cout<<std::format("{}\n",Solution{}.numberOfPairs(nums1,nums2,k));
 }
