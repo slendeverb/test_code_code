802dd86540de613d502710f57a46df1c1eadc84e
stwings <571641990@qq.com>
2024-10-15 15:32:43 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -3,12 +3,42 @@
 using namespace std;
 
 class Solution {
-public:
-    int superEggDrop(int k, int n) {
-        
+   public:
+    int maxHeightOfTriangle(int red, int blue) {
+        int ans = 0;
+        int layer = 1;
+        auto helper = [&layer, &ans](int x, int y) mutable {
+            while (true) {
+                if (layer & 1) {
+                    x -= layer;
+                    if (x < 0) {
+                        ans = std::max(ans, layer - 1);
+                        break;
+                    }
+                } else {
+                    y -= layer;
+                    if (y < 0) {
+                        ans = std::max(ans, layer - 1);
+                        break;
+                    }
+                }
+                layer++;
+            }
+        };
+        helper(red, blue);
+
+        layer = 1;
+        helper(blue, red);
+        return ans;
     }
 };
 
 int main(int argc, char** argv) {
-    std::cout<<std::format("{}\n","12">"111");
+    auto start = std::chrono::system_clock::now();
+    int red{10};
+    int blue{20};
+    std::cout << std::format("{}\n", Solution{}.maxHeightOfTriangle(red, blue));
+    auto end = std::chrono::system_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << std::format("{}\n", duration);
 }
--- test/testC/test2/src/main.c
+++ test/testC/test2/src/main.c
@@ -1,5 +1,32 @@
+#include <math.h>
 #include <stdio.h>
+#include <time.h>
 
-int main(int argc,char** argv){
-    printf("%d\n","12">"111");
+int helper(int x, int y) {
+    for (int i = 1;; i++) {
+        if (i & 1) {
+            x -= i;
+            if (x < 0) {
+                return i - 1;
+            }
+        } else {
+            y -= i;
+            if (y < 0) {
+                return i - 1;
+            }
+        }
+    }
+}
+
+int max_height_of_triangle(int red, int blue) {
+    return fmax(helper(red, blue), helper(blue, red));
+}
+
+int main(int argc, char** argv) {
+    clock_t start = clock();
+    int red = 10;
+    int blue = 20;
+    printf("%d\n", max_height_of_triangle(red, blue));
+    clock_t end = clock();
+    printf("%ld\n", (end - start));
 }
