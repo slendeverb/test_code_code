f27ab55eec8e74e9b24ad570e38e5f5276aea58a
stwings <571641990@qq.com>
2024-10-19 14:16:26 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -1,55 +1,25 @@
-// #include "header.h"
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MOD = 1e9 + 7;
-
 class Solution {
-   public:
-    int numberOfPermutations(int n, vector<vector<int>>& requirements) {
-        std::vector<int> req(n, -1);
-        req[0] = 0;
-        for (auto& p : requirements) {
-            req[p[0]] = p[1];
+public:
+    int minOperations(vector<int>&& nums) {
+        int k=0;
+        for(const auto& x:nums){
+            if(x==k%2){
+                k++;
+            }
         }
-        if (req[0]) {
-            return 0;
-        }
-
-        int m = std::ranges::max(req);
-        std::vector<std::vector<int>> memo(n, std::vector<int>(m + 1, -1));
-        auto dfs = [&](auto&& dfs, int i, int j) -> int {
-            if (i == 0) {
-                return 1;
-            }
-            int& res = memo[i][j];
-            if (res != -1) {
-                return res;
-            }
-            res = 0;
-            if (int r = req[i - 1]; r >= 0) {
-                if (j >= r && j - i <= r) {
-                    res = dfs(dfs, i - 1, r);
-                }
-            } else {
-                for (int k = 0; k <= std::min(i, j); k++) {
-                    res = (res + dfs(dfs, i - 1, j - k)) % MOD;
-                }
-            }
-            return res;
-        };
-        return dfs(dfs,n-1,req[n-1]);
+        return k;
     }
 };
 
 int main(int argc, char** argv) {
-    auto start = std::chrono::system_clock::now();
-    int n{3};
-    std::vector<std::vector<int>> requirements{{2,2},{0,0}};
+    auto start = std::chrono::steady_clock::now();
     auto obj=Solution{};
-    auto ans=obj.numberOfPermutations(n,requirements);
+    auto ans=obj.minOperations({0,1,1,0,0,1,1,1,0,0,0,1,1,1,1,1,1,1,1});
     std::println("{}",ans);
-    auto end = std::chrono::system_clock::now();
+    auto end = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    std::cout << std::format("{}\n", duration);
+    std::cout << std::format("\n{}\n", duration);
 }
