6ebb7ec43b7e069a6e3fd7346c2dcfff59a893ad
stwings <571641990@qq.com>
2024-10-21 20:16:00 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -1,25 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    int minOperations(vector<int>&& nums) {
-        int k=0;
-        for(const auto& x:nums){
-            if(x==k%2){
-                k++;
-            }
-        }
-        return k;
-    }
-};
+
 
 int main(int argc, char** argv) {
-    auto start = std::chrono::steady_clock::now();
-    auto obj=Solution{};
-    auto ans=obj.minOperations({0,1,1,0,0,1,1,1,0,0,0,1,1,1,1,1,1,1,1});
-    std::println("{}",ans);
-    auto end = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    std::cout << std::format("\n{}\n", duration);
+    auto start=std::chrono::steady_clock::now();
+    
+    auto end=std::chrono::steady_clock::now();
+    auto duration=std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+    std::cout<<std::format("\n{}\n",duration);
 }
