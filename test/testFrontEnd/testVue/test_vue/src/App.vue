955dca3157c97df626762a6dbefe5ee3c9d30165
stwings <571641990@qq.com>
2024-10-29 21:01:15 +0800

    update

--- test/testFrontEnd/testVue/test_vue/src/App.vue
+++ test/testFrontEnd/testVue/test_vue/src/App.vue
@@ -1,17 +1,26 @@
 <template>
     <div class="app">
-        <h1>Poem</h1>
+        <h1>诗歌: </h1>
         <p> 相见时难别亦难,东风无力百花残</p>
+        <Person/>
     </div>
 </template>
 
 <script lang="js">
+    import Person from './components/Person.vue';
     export default {
-        name:'App'
+        name:'App',
+        components:{
+            Person,
+        },
     }
 </script>
 
-<style>
+<script setup lang="js">
+
+</script>
+
+<style scoped>
     .app {
         background-color: #fff;
         box-shadow: 0 0 10px;
---
+++ test/testFrontEnd/testVue/test_vue/src/components/Person.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="person">
+        <h2>一辆{{car.brand}}车，价值 {{ car.price }} 万</h2>
+        <button @click="changePrice">修改汽车的价格</button>
+        <hr>
+        <h2>当前求和为: {{ sum }}</h2>
+        <button @click="sumPlusOne">点我sum+1</button>
+    </div>
+</template>
+
+<script lang="js">
+    export default {
+        name:'Person',
+    }
+</script>
+
+<script setup lang="js">
+    import { ref } from 'vue';
+
+    let car=ref({
+        brand:'奔驰',
+        price:100
+    })
+    let sum=ref(0)
+
+    function changePrice(){
+        car.value.price+=10
+    }
+    function sumPlusOne(){
+        sum.value+=1
+    }
+</script>
+
+<style scoped>
+    .person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button {
+        margin: 0 5px;
+    }
+    li {
+        font-size: 150%;
+    }
+</style>
