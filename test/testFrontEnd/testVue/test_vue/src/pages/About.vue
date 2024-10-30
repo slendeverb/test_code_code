f0ffabb2fee96269f4ed5bf6504abc995c5cc17a
stwings <571641990@qq.com>
2024-10-30 23:03:46 +0800

    update

---
+++ test/testFrontEnd/testVue/test_vue/src/pages/About.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="about">
+        <h2>大家好，欢迎来到尚硅谷直播间</h2>
+    </div>
+</template>
+
+<script lang="js">
+    export default {
+        name:'About',
+    }
+</script>
+
+<script setup lang="js">
+    
+</script>
+
+<style scoped>
+    .about{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        color: rgb(85, 84, 84);
+        font-size: 18px;
+    }
+</style>
---
+++ test/testFrontEnd/testVue/test_vue/src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home">
+        <img src="https://www.atguigu.com/images/index_new/logo.png" alt="">
+    </div>
+</template>
+
+<script lang="js">
+    export default {
+        name:'Home',
+    }
+</script>
+
+<script setup lang="js">
+    
+</script>
+
+<style scoped>
+    .home{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+</style>
