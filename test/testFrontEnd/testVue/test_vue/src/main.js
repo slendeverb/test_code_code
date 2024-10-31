7416c51d3465a67a25e58f3208e6e01dfa48636e
stwings <571641990@qq.com>
2024-10-31 14:25:08 +0800

    update

--- test/testFrontEnd/testVue/test_vue/src/main.js
+++ test/testFrontEnd/testVue/test_vue/src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createPinia } from 'pinia'
 
 let app=createApp(App)
-let pinia=createPinia()
-app.use(pinia)
 app.mount('#app')
