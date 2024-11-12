096a49dc03485cfbf2ae7c2fb430c955f116e4f8
stwings <571641990@qq.com>
2024-11-12 21:49:20 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -1,8 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>&& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        long long l=0,r=nums.size()-1;
+        while(l<=r){
+        	long long mid=((r-l)>>1)+l;
+        	if(nums[mid<<1]!=nums[(mid<<1)+1]){
+        		r=mid-1;
+			}else{
+				l=mid+1;
+			}
+		}
+		return nums[l<<1];
+    }
+};
 
 int main(int argc, char** argv) {
-    
+    std::cout<<Solution{}.singleNonDuplicate({1,1,2});
 }
