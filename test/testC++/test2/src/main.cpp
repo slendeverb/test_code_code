93366a35bf7b2901d790ed6f45eb8002980c049c
stwings <571641990@qq.com>
2024-11-17 19:19:17 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -1,14 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-uint64_t factorial(uint64_t n){
-    uint64_t res=1;
-    for(uint64_t i=1;i<=n;i++){
-        res*=i;
-    }
-    return res;
-}
+
 
 int main(){
-	std::cout<<factorial(70)<<"\n";
+	
 }
