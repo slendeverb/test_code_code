911f2f97557848b29b3423f33ffc669a1c8d8c76
stwings <571641990@qq.com>
2025-03-18 20:15:47 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -1,97 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<typename T>
-class SharedCount{
-private:
-	T* ptr;
-	int count;
-	
-	SharedCount(const SharedCount&) = delete;
-	SharedCount& operator=(const SharedCount&) = delete;
-	
-public:
-	SharedCount(T* ptr_): ptr(ptr_),count(1){}
-	
-	~SharedCount(){
-		delete ptr;
-	}
-	
-	void increase(){
-		++count;
-	}
-	
-	void decrease(){
-		if(--count==0){
-			delete this;
-		}
-	}
-	
-	T* get(){
-		return ptr;
-	}
+class Solution {
+   public:
+    int minSwaps(string s) {
+        stack<char> st;
+        int swaps = 0;
+
+        for (char c : s) {
+            if (c == '[') {
+                st.push(c);
+            } else {
+                if (!st.empty() && st.top() == '[') {
+                    st.pop();
+                } else {
+                    swaps++;
+                }
+            }
+        }
+
+        return (swaps + 1) / 2;
+    }
 };
 
-template<typename T>
-class SharedPtr{
-private:
-	T* ptr;
-	SharedCount<T>* countPtr;
-	
-public:
-	SharedPtr(T* ptr_=nullptr): ptr(ptr_){
-		if(ptr_!=nullptr){
-			countPtr=new SharedCount<T>(ptr_);
-		}else{
-			ptr_=nullptr;
-		}
-	}
-	
-	SharedPtr(const SharedPtr& other): ptr(other.ptr),countPtr(other.countPtr){
-		if(countPtr!=nullptr){
-			countPtr->increase();
-		}
-	}
-	
-	SharedPtr(SharedPtr&& other): ptr(other.ptr),countPtr(other.countPtr){
-		other.ptr=nullptr;
-		other.countPtr=nullptr;
-	}
-	
-	~SharedPtr(){
-		if(countPtr){
-			countPtr->decrease();
-		}
-	}
-	
-	T* operator->() const {
-		return ptr;
-	}
-	
-	T& operator*() const {
-		return *ptr;
-	}
-	
-	void reset(T* p=nullptr){
-		if(p!=ptr){
-			if(countPtr){
-			    countPtr->decrease();
-		    }
-		}
-		ptr=p;
-		if(p){
-			countPtr=new SharedCount<T>(p);
-		}else{
-			countPtr=nullptr;
-		}
-	}
-	
-	T* get() const {
-		return ptr;
-	}
-};
-
-int main() {
-	SharedPtr<std::vector<int>> ptr(new std::vector<int>(10,0));
-    std::cout<<ptr->front()<<"\n";
-}
+int main() {}
