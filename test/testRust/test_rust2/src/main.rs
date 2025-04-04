176f843c33dfd29195793c0d9f8666555aff3b9e
stwings <571641990@qq.com>
2025-04-04 17:57:25 +0800

    update

--- test/testRust/test_rust2/src/main.rs
+++ test/testRust/test_rust2/src/main.rs
@@ -1,70 +1,160 @@
 impl Solution {
-    pub fn most_points(questions:Vec<Vec<i32>>)->i64{
-        let n=questions.len();
-        let mut f=vec![0i64;n+1];
-        for i in 0..f.len()-1{
-            f[i]=questions[i][0] as i64;
+    pub fn lca_deepest_leaves(
+        root: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut ans = None;
+        let mut max_depth = -1;
+
+        fn dfs(
+            node: &Option<Rc<RefCell<TreeNode>>>,
+            depth: i32,
+            max_depth: &mut i32,
+            ans: &mut Option<Rc<RefCell<TreeNode>>>,
+        ) -> i32 {
+            if let Some(node) = node {
+                let x = node.borrow();
+                let left_max_depth = dfs(&x.left, depth + 1, max_depth, ans);
+                let right_max_depth = dfs(&x.right, depth + 1, max_depth, ans);
+                if left_max_depth == right_max_depth && left_max_depth == *max_depth {
+                    *ans = Some(node.clone());
+                }
+                left_max_depth.max(right_max_depth)
+            } else {
+                *max_depth = (*max_depth).max(depth);
+                depth
+            }
         }
-        for i in (0..=n-1).rev() {
-            let skip_idx=if i+1>=n {n} else {i+1};
-            let choose_idx=if i+questions[i][1] as usize+1>=n {n} else {i+questions[i][1] as usize+1};
-            f[i]=f[i].max(f[skip_idx]).max(f[choose_idx]+questions[i][0] as i64);
-        }
-        f[0]
+
+        dfs(&root, 0, &mut max_depth, &mut ans);
+        ans
     }
 }
 
+#[allow(dead_code)]
 struct Solution {}
 
 fn main() {
-    let mut questions=vec![vec![0;2];100000];
-    for i in 0..questions.len(){
-        questions[i][0]=i as i32;
-        questions[i][1]=i as i32;
-    }
-    let result=Solution::most_points(questions);
-    print!("{:?}\n",result);
+    let input = "[0,1,2,3,4,5,6,7,8,9,10,11,12,13]";
+    let nums = parse_tree_str::<i64>(input);
+
+    let root = build_tree(&nums);
+    let result = Solution::lca_deepest_leaves(root);
+    println!("{:#?}", result);
 }
 
 #[allow(dead_code)]
 fn convert_two_dim<T, const M: usize, const N: usize>(arr: [[T; M]; N]) -> Vec<Vec<T>> {
-    let mut ret = Vec::with_capacity(N);
-    for v in arr.into_iter() {
-        ret.push(v.into_iter().collect());
-    }
-    ret
+    arr.into_iter()
+        .map(|inner| inner.into_iter().collect())
+        .collect()
 }
 
 #[allow(dead_code)]
 fn convert_three_dim<T, const M: usize, const N: usize, const P: usize>(
     arr: [[[T; M]; N]; P],
 ) -> Vec<Vec<Vec<T>>> {
-    let mut ret = Vec::with_capacity(P);
-    for v in arr.into_iter() {
-        let mut tmp = Vec::with_capacity(N);
-        for w in v.into_iter() {
-            tmp.push(w.into_iter().collect());
-        }
-        ret.push(tmp);
-    }
-    ret
+    arr.into_iter()
+        .map(|mid| {
+            mid.into_iter()
+                .map(|inner| inner.into_iter().collect())
+                .collect()
+        })
+        .collect()
 }
 
 #[allow(dead_code)]
 fn convert_four_dim<T, const M: usize, const N: usize, const P: usize, const Q: usize>(
     arr: [[[[T; M]; N]; P]; Q],
 ) -> Vec<Vec<Vec<Vec<T>>>> {
-    let mut ret = Vec::with_capacity(Q);
-    for v in arr.into_iter() {
-        let mut tmp1 = Vec::with_capacity(P);
-        for w in v.into_iter() {
-            let mut tmp2 = Vec::with_capacity(N);
-            for u in w.into_iter() {
-                tmp2.push(u.into_iter().collect());
-            }
-            tmp1.push(tmp2);
+    arr.into_iter()
+        .map(|outer| {
+            outer
+                .into_iter()
+                .map(|mid| {
+                    mid.into_iter()
+                        .map(|inner| inner.into_iter().collect())
+                        .collect()
+                })
+                .collect()
+        })
+        .collect()
+}
+
+#[allow(dead_code)]
+use std::cell::RefCell;
+use std::rc::Rc;
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode<T = i64> {
+    pub val: T,
+    pub left: Option<Rc<RefCell<TreeNode<T>>>>,
+    pub right: Option<Rc<RefCell<TreeNode<T>>>>,
+}
+
+#[allow(dead_code)]
+impl<T> TreeNode<T> {
+    #[inline]
+    pub fn new(val: T) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
         }
-        ret.push(tmp1);
     }
-    ret
+}
+
+#[allow(dead_code)]
+use std::str::FromStr;
+fn parse_tree_str<T: FromStr>(s: &str) -> Vec<Option<T>> {
+    let s = s.trim().trim_start_matches('[').trim_end_matches(']');
+
+    s.split(',')
+        .map(|part| {
+            let part = part.trim();
+            if part == "null" {
+                None
+            } else {
+                part.parse().ok()
+            }
+        })
+        .collect()
+}
+
+#[allow(dead_code)]
+type Tree<T> = Option<Rc<RefCell<TreeNode<T>>>>;
+fn build_tree<T: Clone + Default>(nums: &[Option<T>]) -> Tree<T> {
+    if nums.is_empty() || nums[0].is_none() {
+        return None;
+    }
+
+    let root = Rc::new(RefCell::new(TreeNode::new(
+        nums[0].clone().unwrap_or_default(),
+    )));
+    let mut queue = std::collections::VecDeque::new();
+    queue.push_back(Rc::clone(&root));
+    let mut i = 1;
+
+    while !queue.is_empty() && i < nums.len() {
+        let node = queue.pop_front().unwrap();
+        let mut node = node.borrow_mut();
+
+        if i < nums.len() {
+            if let Some(val) = nums[i].clone() {
+                let left = Rc::new(RefCell::new(TreeNode::new(val)));
+                node.left = Some(Rc::clone(&left));
+                queue.push_back(left);
+            }
+            i += 1;
+        }
+
+        if i < nums.len() {
+            if let Some(val) = nums[i].clone() {
+                let right = Rc::new(RefCell::new(TreeNode::new(val)));
+                node.right = Some(Rc::clone(&right));
+                queue.push_back(right);
+            }
+            i += 1;
+        }
+    }
+
+    Some(root)
 }
