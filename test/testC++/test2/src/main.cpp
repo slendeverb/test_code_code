4a9740fc6617367f1ec5827c0f34027a485f24a0
stwings <571641990@qq.com>
2025-04-28 14:56:05 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -1,24 +1,6 @@
 #include "header.h"
 using namespace std;
 
-class Solution {
-public:
-    long long countGood(vector<int>& nums, int k) {
-        long long ans=0;
-        std::unordered_map<int,int> cnt;
-        int pairs=0,left=0;
-        for(int x:nums){
-            pairs+=cnt[x]++;
-            while(pairs>=k){
-                pairs-=--cnt[nums[left]];
-                left++;
-            }
-            ans+=left;
-        }
-        return ans;
-    }
-};
-
 // 读取CSV
 vector<vector<string>> readCSV(string filename) {
     vector<vector<string>> data;
