9f87f1b6b5fe55207e81c3a74b44c5a1ad8d9457
stwings <571641990@qq.com>
2025-05-09 16:02:57 +0800

    update

--- test/testPython/test/src/test.py
+++ test/testPython/test/src/test.py
@@ -6,20 +6,30 @@ import matplotlib
 
 matplotlib.rc("font",family="Microsoft YaHei")
 
+# pd.set_option('display.max_row',None)
+# pd.set_option('display.max_column',None)
+
 data = pd.read_csv("D:/Tencent Files/QQ Files/Tencent Files/571641990/FileRecv/" \
     "气象信息系统工程/实验/实验1/实验1 2006年至2016年塞格德的气象数据/weatherHistory.csv")
 
 print(data)
 
-plt.figure(figsize=(10,6))
-
 data.drop_duplicates(subset=["Formatted Date"],inplace=True)
 data.sort_values(by="Formatted Date",inplace=True)
 
-plt.plot(data["Formatted Date"][0:100],data["Apparent Temperature (C)"][0:100],color="blue",label="温度")
+date_list:list[str]=[]
+for x in data["Formatted Date"].values:
+    assert(type(x) is str)
+    pos=x.find("-")
+    date_list.append(x[pos+1:pos+3])
 
+start_pos=0
+end_pos=data.__len__()
+print(end_pos)
+plt.figure(figsize=(10,6))
+plt.plot(date_list[start_pos:end_pos],data["Apparent Temperature (C)"][start_pos:end_pos],'.',color="blue",label="温度")
 plt.xlabel("日期")
 plt.ylabel("温度")
 plt.title("塞格德2006年至2016年温度变化")
 plt.legend()
-plt.show()
+# plt.show()
