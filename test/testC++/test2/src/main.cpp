457fa41560890467f26f832abf39a6cd14b33f91
stwings <571641990@qq.com>
2025-08-25 16:23:48 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -3,14 +3,39 @@ using namespace std;
 
 class Solution {
 public:
-    vector<string> removeSubfolders(vector<string>& folder) {
-        std::ranges::sort(folder);
-        std::vector<std::string> ans{folder[0]};
-        for(int i=1;i<folder.size();i++){
-            std::string& s=folder[i];
-            std::string& last=ans.back();
-            if(!s.starts_with(last) || s[last.size()]!='/'){
-                ans.emplace_back(s);
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        std::vector<int> ans;
+        int i=0,j=0;
+        int m=mat.size();
+        int n=mat[0].size();
+        while(i<m && j<n){
+            while(i>=0){
+                ans.emplace_back(mat[i][j]);
+                if(i-1<0 || j+1>=n){
+                    break;
+                }
+                i-=1;j+=1;
+            }
+            if(j+1<n){
+                j+=1;
+            }else if(i+1<m){
+                i+=1;
+            }else{
+                break;
+            }
+            while(i<m){
+                ans.emplace_back(mat[i][j]);
+                if(i+1>=m || j-1<0){
+                    break;
+                }
+                i+=1;j-=1;
+            }
+            if(i+1<m){
+                i+=1;
+            }else if(j+1<n){
+                j+=1;
+            }else{
+                break;
             }
         }
         return ans;
@@ -18,9 +43,9 @@ public:
 };
 
 int main() {
-    std::vector<std::string> folder{"/a","/a/b","/c/d","/c/d/e","/c/f"};
-    auto result=Solution{}.removeSubfolders(folder);
-    for(const auto& x:result){
-        std::cout<<std::format("{}\n",x);
+    std::vector<std::vector<int>> mat{{1,2},{4,5},{7,8}};
+    auto result=Solution{}.findDiagonalOrder(mat);
+    for(int i=0;i<result.size();i++){
+        std::cout<<std::format("{} ",result[i]);
     }
 }
