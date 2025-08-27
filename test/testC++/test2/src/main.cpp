e7c50727828c1f0074769f7026a4eb807e6b8879
stwings <571641990@qq.com>
2025-08-27 12:37:39 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -2,25 +2,59 @@
 using namespace std;
 
 class Solution {
-public:
-    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-        double diagonal=0.0;
-        int area=0;
-        for(int i=0;i<dimensions.size();i++){
-            int x=dimensions[i][0];
-            int y=dimensions[i][1];
-            double temp_diagonal=std::sqrt(x*x+y*y);
-            if(std::fabs(temp_diagonal-diagonal)<1e-6){
-                area=std::max(area,x*y);
-            }else if(temp_diagonal>diagonal){
-                diagonal=temp_diagonal;
-                area=x*y;
+    static constexpr int DIRS[4][2] = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+
+   public:
+    int lenOfVDiagonal(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        std::vector memo(n, std::vector<std::array<int, 4>>(m));
+
+        auto dfs = [&](this auto&& dfs, int i, int j, int k, bool rotated, int target) -> int {
+            i += DIRS[k][0];
+            j += DIRS[k][1];
+
+            if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != target) {
+                return 0;
+            }
+            if (rotated && memo[i][j][k]) {
+                return memo[i][j][k];
+            }
+
+            int res = dfs(i, j, k, rotated, 2 - target) + 1;
+            if (rotated) {
+                return memo[i][j][k] = res;
+            }
+
+            int maxs[4] = {n - i, j + 1, i + 1, m - j};
+            k = (k + 1) % 4;
+            if (std::min(maxs[k], maxs[(k + 3) % 4]) > res) {
+                res = std::max(res, dfs(i, j, k, true, 2 - target) + 1);
+            }
+            return res;
+        };
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] != 1) {
+                    continue;
+                }
+
+                int maxs[4] = {n - i, j + 1, i + 1, m - j};
+                for (int k = 0; k < 4; k++) {
+                    if (maxs[k] > ans) {
+                        ans = std::max(ans, dfs(i, j, k, false, 2) + 1);
+                    }
+                }
             }
         }
-        return area;
+        return ans;
     }
 };
 
 int main() {
-    
+    std::vector<std::vector<int>> grid{
+        {2, 2, 1, 2, 2}, {2, 0, 2, 2, 0}, {2, 0, 1, 1, 0}, {1, 0, 2, 2, 2}, {2, 0, 0, 2, 2}};
+    auto result = Solution{}.lenOfVDiagonal(grid);
+    std::cout << std::format("{}\n", result);
 }
