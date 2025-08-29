5bdfd34e9b808859d2c874689989eb7619f40640
stwings <571641990@qq.com>
2025-08-30 02:18:31 +0800

    update

--- test/testC++/test2/src/main.cpp
+++ test/testC++/test2/src/main.cpp
@@ -3,41 +3,47 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        std::vector ans(n,std::vector<int>(n));
-        int i=n-1,j=0;
-        int cnt_diagonal=0;
-        while(i>=0 && i<n && j>=0 && j<n){
-            std::vector<int> temp_vec;
-            int p=i,q=j;
-            while(p<n && q<n){
-                temp_vec.emplace_back(grid[p][q]);
-                p++,q++;
-            }
-            cnt_diagonal++;
-            if(cnt_diagonal<=n){
-                std::sort(temp_vec.begin(),temp_vec.end(),std::greater<>{});
-            }else{
-                std::sort(temp_vec.begin(),temp_vec.end(),std::less<>{});
-            }
-            p=i,q=j;
-            int cnt=0;
-            while(p<n && q<n){
-                ans[p][q]=temp_vec[cnt++];
-                p++,q++;
-            }
+    bool isValidSudoku(vector<string>& board) {
+        int n=board.size();
+        std::vector<std::unordered_set<int>> row(n),col(n);
+        std::vector<std::vector<std::unordered_set<int>>> square(n/3,std::vector<std::unordered_set<int>>(n/3));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                char x=board[i][j];
+                if(x=='.'){
+                    continue;
+                }
+                
+                if(row[i].contains(x)){
+                    return false;
+                }
+                row[i].insert(x);
+                
+                if(col[j].contains(x)){
+                    return false;
+                }
+                col[j].insert(x);
 
-            if(i-1>=0){
-                i--;
-            }else{
-                j++;
+                if(square[i/3][j/3].contains(x)){
+                    return false;
+                }
+                square[i/3][j/3].insert(x);
             }
         }
-        return ans;
+        return true;
     }
 };
 
 int main() {
-    
+    std::vector<std::string> board{"53..7....",
+                                    "6..195...",
+                                    ".98....6.",
+                                    "8...6...3",
+                                    "4..8.3..1",
+                                    "7...2...6",
+                                    ".6....28.",
+                                    "...419..5",
+                                    "....8..79"};
+    auto result=Solution{}.isValidSudoku(board);
+    std::print("{}\n",result);
 }
